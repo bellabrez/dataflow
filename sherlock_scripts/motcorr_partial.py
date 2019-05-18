76732c2083bfc01b835dd96c7ad8c2957cbf4cf0
lukebrez <brezovec@stanford.edu>
2019-05-17 17:36:40 -0700

    auto motcorr

--- sherlock_scripts/motcorr_partial.py
+++ sherlock_scripts/motcorr_partial.py
@@ -1,30 +1,41 @@
 import sys
 import os
 import BigBadBrain as bbb
+import warnings
+warnings.filterwarnings("ignore")
 
 sys.path.insert(0, '/home/users/brezovec/.local/lib/python3.6/site-packages/lib/python/')
 import ants
 
 def main(args):
-	directory = args[0]
-	motcorr_directory = args[1]
-	master_path = args[2]
-	slave_path = args[3]
-	master_path_mean = args[4]
-	vol_start = args[5]
-	vol_end = args[6]
+    directory = args[0]
+    motcorr_directory = args[1]
+    master_path = args[2]
+    slave_path = args[3]
+    master_path_mean = args[4]
+    vol_start = args[5]
+    vol_end = args[6]
 
-	master_brain = ants.from_numpy(bbb.load_numpy_brain(master_path))
-	slave_brain = ants.from_numpy(bbb.load_numpy_brain(slave_path))
-	mean_brain = ants.from_numpy(bbb.load_numpy_brain(master_path_mean))
+    # For the sake of memory, lets try to load only the part of the brain we will need.
 
-	bbb.motion_correction(master_brain,
-						  slave_brain,
-						  directory,
-						  motcorr_directory,
-						  meanbrain=mean_brain,
-						  start_volume=vol_start,
-						  end_volume=vol_end)
+
+
+    master_brain = load_partial_brain(master_path,0,10)
+    slave_brain = load_partial_brain(slave_path,0,10)
+    mean_brain = ants.from_numpy(bbb.load_numpy_brain(master_path_mean))
+
+    bbb.motion_correction(master_brain,
+                          slave_brain,
+                          directory,
+                          motcorr_directory,
+                          meanbrain=mean_brain,
+                          start_volume=vol_start,
+                          end_volume=vol_end)
+
+def load_partial_brain(file, start, stop):
+    brain = nib.load(file).dataobj[:,:,:,start:stop]
+    brain = ants.from_numpy(np.asarray(np.squeeze(brain), 'float64'))
+    return brain
 
 if __name__ == '__main__':
-	main(sys.argv[1:])
+    main(sys.argv[1:])
