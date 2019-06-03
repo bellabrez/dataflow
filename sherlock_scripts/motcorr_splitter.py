1a5769c3344c1fd49814db083a67a53e579caaab
lukebrez <brezovec@stanford.edu>
2019-06-03 16:08:02 -0700

    created motcorr stitcher skeleton.

--- sherlock_scripts/motcorr_splitter.py
+++ sherlock_scripts/motcorr_splitter.py
@@ -32,8 +32,8 @@ def main(args):
 
     #Start fleet of motcorr_partial.sh, giving each the correct portion of data
 
-    num_vols = 2 ###### FOR TESTING; REMOVE. ######
-    step = 1 ###### put back to 100 ######
+    num_vols = 4 ###### FOR TESTING; REMOVE. ######
+    step = 2 ###### put back to 100 ######
     job_ids = []
     for i in range(0,num_vols,step):
         vol_start = i
