df22dbebe9d47ee57797f0cabf980fd75eac42c1
lukebrez <brezovec@stanford.edu>
2019-06-04 15:33:21 -0700

    some fictrac and visual transfer work.

---
+++ sherlock_scripts/check_for_flag.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=check_transfer
+#SBATCH --partition=trc
+#SBATCH --time=0:05:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+
+ml python/3.6.1
+
+python3 /home/users/brezovec/projects/dataflow/sherlock_scripts/check_for_flag.py
