c549d7537dd7a9312d830f6c0f17161794defde0
lukebrez <brezovec@stanford.edu>
2019-06-04 15:46:04 -0700

    added modules to sh.

--- sherlock_scripts/build_fly.sh
+++ sherlock_scripts/build_fly.sh
@@ -5,7 +5,12 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 
+ml gcc/6.3.0
 ml python/3.6.1
+ml py-numpy/1.14.3_py36
+ml py-pandas/0.23.0_py36
+ml viz
+ml py-scikit-learn/0.19.1_py36
 
 directory="$1"
 
--- sherlock_scripts/check_for_flag.sh
+++ sherlock_scripts/check_for_flag.sh
@@ -5,6 +5,11 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 
+ml gcc/6.3.0
 ml python/3.6.1
+ml py-numpy/1.14.3_py36
+ml py-pandas/0.23.0_py36
+ml viz
+ml py-scikit-learn/0.19.1_py36
 
 python3 /home/users/brezovec/projects/dataflow/sherlock_scripts/check_for_flag.py
