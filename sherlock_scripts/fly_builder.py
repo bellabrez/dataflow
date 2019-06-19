74f5385881e4285d6aa4f232f06b9a303bfc7c03
Luke Brezovec <brezovec@stanford.edu>
2019-06-18 18:34:36 -0700

    switching from xml to json

--- sherlock_scripts/fly_builder.py
+++ sherlock_scripts/fly_builder.py
@@ -7,6 +7,7 @@ from xml.etree import ElementTree as ET
 from lxml import etree, objectify
 import bigbadbrain as bbb
 import pandas as pd
+import json
 
 def main(args):
     ### Move folders from imports to fly dataset - need to restructure folders ###
@@ -502,127 +503,90 @@ def get_new_fly_number(target_path):
     current_fly_number = oldest_fly + 1
     return current_fly_number
 
+def load_json(file):
+    with open(file, 'r') as f:
+        data = json.load(f)
+    return data
+
 def add_fly_to_metadata(fly_folder):
     # For each fly, need to handle multiple experiments
     
     # Load fly metadata
-    xml_file = os.path.join(fly_folder, 'fly.xml')
-    fly = load_xml(xml_file)
+    fly_file = os.path.join(fly_folder, 'fly.json')
+    fly_data = load_json(fly_file)
     
-    # Need to add each experiment
-    # First get correct expt order based on expt time
-    expts = []
+    # Add each experiment
     for item in os.listdir(fly_folder):
-        if os.path.isdir(os.path.join(fly_folder, item)):
-            if 'anatomy' in item or 'functional' in item:
-                expts.append(item)
-    
-    expts_sorted, datetimes_sorted = order_expts(fly_folder, expts)
-    
-    for i in range(len(expts)):
-        expt_folder = os.path.join(fly_folder, expts_sorted[i])
-        add_expt_to_metadata(fly, expt_folder, datetimes_sorted[i], i)
-                
-def order_expts(fly_folder, expts):
-    # For each expt, get it's time
-    datetimes = []
-    print('expts pre-sort: {}'.format(expts))
-    sys.stdout.flush()
-    for expt in expts:
-        if 'anatomy' in expt:
-            xml_file = os.path.join(fly_folder, expt, 'anatomy.xml')
-            datetime_str,_,_ = get_datetime_from_xml(xml_file)
-        if 'functional' in expt:
-            xml_file = os.path.join(fly_folder, expt, 'imaging', 'functional.xml')
-            datetime_str,_,_ = get_datetime_from_xml(xml_file)
-        datetimes.append(datetime_str)
-    
-    expts_sorted = [x for _,x in sorted(zip(datetimes,expts))]
-    print('expts_sorted: {}'.format(expts_sorted))
-    datetimes_sorted = sorted(datetimes)
-    print('datetimes_sorted: {}'.format(datetimes_sorted))
-    sys.stdout.flush()
-    return expts_sorted, datetimes_sorted
+        if 'func' in item:
+            expt_folder = os.path.join(fly_folder, item)
+            add_expt_to_metadata(fly_data, expt_folder)
 
-def add_expt_to_metadata(fly, expt_folder, datetime, expt_id):    
+def add_expt_to_metadata(fly_data, expt_folder):    
     # Load expt metadata
-    xml_file = os.path.join(expt_folder, 'expt.xml')
-    expt = load_xml(xml_file)
+    expt_file = os.path.join(expt_folder, 'expt.json')
+    expt_data = load_json(expt_file)
     
+    # Load scan metadata
+    scan_file = os.path.join(expt_folder, 'imaging', 'scan.json')
+    scan_data = load_json(scan_file)
+
     # Load visual metadata
     try:
-        xml_file = os.path.join(expt_folder, 'visual', 'visual.xml')
-        visual = load_xml(xml_file)
+        visual_file = os.path.join(expt_folder, 'visual', 'visual.json')
+        visual_data = load_json(visual_file)
     except:
-        visual = None
+        visual_data = None
         print('Visual metadata not found.')
         sys.stdout.flush()
     
+    fictrac_data = None
     # Load fictrac metadata
-    try:
-        xml_file = os.path.join(expt_folder, 'fictrac', 'fictrac.xml')
-        fictrac = load_xml(xml_file)
+    # try:
+    #     xml_file = os.path.join(expt_folder, 'fictrac', 'fictrac.xml')
+    #     fictrac = load_xml(xml_file)
         
-        # UPDATE THIS WHEN FICTRAC META CONTAINS REAL INFO
-        fictrac = True
-    except:
-        fictrac = None
-        print('Fictrac metadata not found.')
-        sys.stdout.flush()
-    
-    # Load scan metadata
-    # First try to load from imaging folder (in the case of functional)
-    try:
-        xml_file = os.path.join(expt_folder, 'imaging', 'scan.xml')
-        scan = load_xml(xml_file)
-    # Next try to load from expt folder (in the case of anatomy)
-    except:
-        xml_file = os.path.join(expt_folder, 'scan.xml')
-        scan = load_xml(xml_file)
+    #     # UPDATE THIS WHEN FICTRAC META CONTAINS REAL INFO
+    #     fictrac = True
+    # except:
+    #     fictrac = None
+    #     print('Fictrac metadata not found.')
+    #     sys.stdout.flush()
     
     # Load experiment master dataframe
     master_expt_path = '/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/master_expt.pkl'
     master_expt = pd.read_pickle(master_expt_path)
     
-    # Get scan type
-    if 'functional' in expt_folder:
-        scan_type = 'functional'
-    if 'anatomy' in expt_folder:
-        scan_type = 'anatomy'
-    
-    # Get datetime
-    date = datetime.split('-')[0]
-    time = datetime.split('-')[1]
-    
     # Get fly_id
     fly_folder = os.path.split(os.path.split(expt_folder)[0])[-1]
     fly_id = fly_folder.split('_')[-1]
 
+    # Get expt_id
+    expt_id = expt_folder.split('_')[-1]
+
     # Append the new row
     new_row = {'fly_id': fly_id,
                'expt_id': expt_id,
-               'date': date,
-               'time': time,
-               'circ_on': str(fly.circadian_on),
-               'circ_off': str(fly.circadian_off),
-               'gender': str(fly.gender),
-               'age': str(fly.age),
-               'genotype': str(fly.genotype),
-               'brain_area': str(expt.brain_area),
-               'expt_notes': str(expt.notes),
-               'scan_type': str(scan_type),
-               'temp': str(fly.temp),
-               'fly_notes': str(fly.notes),
-               'laser_power': str(scan.power),
-               'PMT_green': str(scan.PMT_green),
-               'PMT_red': str(scan.PMT_red),
-               'x_voxel_size': str(scan.x_voxel_size),
-               'y_voxel_size': str(scan.y_voxel_size),
-               'z_voxel_size': str(scan.z_voxel_size)}
+               'date': fly_data['date'],
+               'time': expt_data['time'],
+               'circ_on': fly_data['circadian_on'],
+               'circ_off': fly_data['circadian_off'],
+               'gender': fly_data['gender'],
+               'age': fly_data['age'],
+               'genotype': fly_data['genotype'],
+               'brain_area': expt_data['brain_area'],
+               'expt_notes': expt_data['notes'],
+               'temp': fly_data['temp'],
+               'fly_notes': fly_data['notes'],
+               'laser_power': scan_data['power'],
+               'PMT_green': scan_data['PMT_green'],
+               'PMT_red': scan_data['PMT_red'],
+               'x_voxel_size': scan_data['x_voxel_size'],
+               'y_voxel_size': scan_data['y_voxel_size'],
+               'z_voxel_size': scan_data['z_voxel_size']}
     
-    if visual is not None:
-        new_row['visual_stimuli'] = str(visual.unique_stimuli)
-    if fictrac is not None:
+    if visual_data is not None:
+        new_row['visual_stimuli'] = visual_data
+    if fictrac_data is not None:
         new_row['fictrac'] = fictrac
                
     master_expt = master_expt.append(new_row, ignore_index=True)
--- sherlock_scripts/json_test.py
+++ sherlock_scripts/json_test.py
@@ -4,7 +4,17 @@ mydict = {'gender': 'female',
           'age': 7,
           'genotype': '99/1_GCaMP6f',
           'circadian_on': 1,
-          'circadian_on'}
+          'circadian_off': 13,
+          'temp': 25,
+          'notes': ''}
+
+mylist = [{'name': 'SineGrating', 'angle': 90, 'period': 40, 'rate': 100, 'color': 1},
+{'name': 'SineGrating', 'angle': 0, 'period': 40, 'rate': 100, 'color': 1},
+{'name': 'SineGrating', 'angle': 270, 'period': 40, 'rate': 100, 'color': 1},
+{'name': 'SineGrating', 'angle': 180, 'period': 40, 'rate': 100, 'color': 1}]
 
 with open('result.json', 'w') as fp:
-    json.dump(mydict, fp, indent=4)
+    json.dump(mylist, fp, indent=4)
+
+with open('result.json', 'w') as fp:
+    json.dump(mylist, fp, indent=4)
