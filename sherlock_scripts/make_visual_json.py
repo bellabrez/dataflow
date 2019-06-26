ad170f031ab140f873810210934a58ba7be08319
Luke Brezovec <brezovec@stanford.edu>
2019-06-25 22:36:11 -0700

    make visual json

--- sherlock_scripts/make_visual_json.py
+++ sherlock_scripts/make_visual_json.py
@@ -6,9 +6,9 @@ import bigbadbrain as bbb
 def main():
 
     root_path = '/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/'
-    flies = [36]
+    flies = [33,34,35]
     for fly in flies:
-        fly_folder = os.path.join(root_path, 'fly' + str(fly))
+        fly_folder = os.path.join(root_path, 'fly_' + str(fly))
         visual_folders = [os.path.join(fly_folder,x,'visual') for x in os.listdir(fly_folder) if 'func' in x]
         for visual_folder in visual_folders:
             stimuli, unique_stimuli = bbb.load_visual_stimuli_data(visual_folder)
