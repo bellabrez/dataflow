c1408cd11ac7c79e09cae7011b82dc66acbe8af6
Luke Brezovec <brezovec@stanford.edu>
2019-06-27 00:56:10 -0700

    updating fly builder to work with jsons.

--- sherlock_scripts/make_all_scan_jsons.py
+++ sherlock_scripts/make_all_scan_jsons.py
@@ -62,12 +62,12 @@ def create_imaging_json(xml_source_file):
                 if index_num == '1':
                     source_data['PMT_green'] = index.get('value')
         if key == 'pixelsPerLine':
-            source_data['width'] = int(statevalue.get('value'))
+            source_data['x_dim'] = int(statevalue.get('value'))
         if key == 'linesPerFrame':
-            source_data['height'] = int(statevalue.get('value'))
+            source_data['y_dim'] = int(statevalue.get('value'))
     sequence = source.findall('Sequence')[0]
     last_frame = sequence.findall('Frame')[-1]
-    source_data['depth'] = last_frame.get('index')
+    source_data['z_dim'] = int(last_frame.get('index'))
 
     # Save data
     with open(os.path.join(os.path.split(xml_source_file)[0], 'scan.json'), 'w') as f:
