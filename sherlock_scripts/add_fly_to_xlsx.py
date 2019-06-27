1a46e31638c94caf7995556ef5335aa277159163
Luke Brezovec <brezovec@stanford.edu>
2019-06-27 02:27:22 -0700

    updating fly builder to work with jsons.

--- sherlock_scripts/add_fly_to_xlsx.py
+++ sherlock_scripts/add_fly_to_xlsx.py
@@ -3,83 +3,112 @@ from openpyxl import load_workbook
 from openpyxl.styles import Font, Fill
 import json
 import os
+import bigbadbrain as bbb
 
 def load_json(file):
     with open(file, 'r') as f:
         data = json.load(f)
     return data
 
-filename='/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/master_2P.xlsx'
-wb = load_workbook(filename=filename, read_only=False)
-ws = wb.active
-#ws = wb['big_data']
-#wb = Workbook()
-#wb = wb.load()
-# grab the active worksheet
-#col = ws.column_dimensions['B']
-#col.font = Font(bold=True)
-# Data can be assigned directly to cells
-#ws['A1'] = 42
+def main():
+    filename='/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/master_2P.xlsx'
+    wb = load_workbook(filename=filename, read_only=False)
+    ws = wb.active
+    #ws = wb['big_data']
+    #wb = Workbook()
+    #wb = wb.load()
+    # grab the active worksheet
+    #col = ws.column_dimensions['B']
+    #col.font = Font(bold=True)
+    # Data can be assigned directly to cells
+    #ws['A1'] = 42
 
-# Rows can also be appended
-fly_folder = '/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/fly_36'
-fly_file = os.path.join(fly_folder, 'fly.json')
-fly_data = load_json(fly_file)
+    root_directory = '/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/'
+    fly_folders = [os.path.join(root_directory,x) for x in os.listdir(root_directory) if 'fly' in x]
+    bbb.sort_nicely(fly_folders)
+    #fly_folder = '/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/fly_36'
+    for fly_folder in fly_folders:
+        print(fly_folder)
+        # If no fly.json, just skip (atleast one fly in empty for some reason)
+        try:
+            fly_file = os.path.join(fly_folder, 'fly.json')
+            fly_data = load_json(fly_file)
+        except:
+            continue
 
-expt_folder = os.path.join(fly_folder, 'func_0')
-expt_file = os.path.join(expt_folder, 'expt.json')
-expt_data = load_json(expt_file)
+        expt_folders = []
+        expt_folders = [os.path.join(fly_folder,x) for x in os.listdir(fly_folder) if 'func' in x]
+        bbb.sort_nicely(expt_folders)
+        for expt_folder in expt_folders:
 
-scan_file = os.path.join(expt_folder, 'imaging', 'scan.json')
-scan_data = load_json(scan_file)
+            expt_file = os.path.join(expt_folder, 'expt.json')
+            expt_data = load_json(expt_file)
 
-visual_file = os.path.join(expt_folder, 'visual', 'visual.json')
-try:
-    visual_data = load_json(visual_file)
-    visual_input = visual_data[0]['name'] + '_{} modes'.format(len(visual_data))
-except:
-    visual_input = None
+            # Occasionally a fly may not have an imaging folder (if only fictrac was recorded for example)
+            try:
+                scan_file = os.path.join(expt_folder, 'imaging', 'scan.json')
+                scan_data = load_json(scan_file)
+            except:
+                scan_data['laser_power'] = None
+                scan_data['PMT_green'] = None
+                scan_data['PMT_red'] = None
+                scan_data['x_dim'] = None
+                scan_data['y_dim'] = None
+                scan_data['z_dim'] = None
+                scan_data['x_voxel_size'] = None
+                scan_data['y_voxel_size'] = None
+                scan_data['z_voxel_size'] = None
 
-# Get fly_id
-fly_folder = os.path.split(os.path.split(expt_folder)[0])[-1]
-fly_id = fly_folder.split('_')[-1]
+            visual_file = os.path.join(expt_folder, 'visual', 'visual.json')
+            try:
+                visual_data = load_json(visual_file)
+                visual_input = visual_data[0]['name'] + ' ({})'.format(len(visual_data))
+            except:
+                visual_input = None
 
-# Get expt_id
-expt_id = expt_folder.split('_')[-1]
+            # Get fly_id
+            fly_folder = os.path.split(os.path.split(expt_folder)[0])[-1]
+            fly_id = fly_folder.split('_')[-1]
 
-# Append the new row
-new_row = [int(fly_id),
-           int(expt_id),
-           fly_data['date'],
-           expt_data['brain_area'],
-           fly_data['genotype'],
-           visual_input,
-           fly_data['notes'],
-           expt_data['notes'],
-           expt_data['time'],
-           fly_data['circadian_on'],
-           fly_data['circadian_off'],
-           fly_data['gender'],
-           fly_data['age'],
-           fly_data['temp'],
-           scan_data['laser_power'],
-           scan_data['PMT_green'],
-           scan_data['PMT_red'],
-           scan_data['x_dim'],
-           scan_data['y_dim'],
-           scan_data['z_dim'],
-           scan_data['x_voxel_size'],
-           scan_data['y_voxel_size'],
-           scan_data['z_voxel_size']]
+            # Get expt_id
+            expt_id = expt_folder.split('_')[-1]
 
-#if visual_data is not None:
-#    new_row['visual_stimuli'] = visual_data
-#if fictrac_data is not None:
-#    new_row['fictrac'] = fictrac
+            # Append the new row
+            new_row = []
+            new_row = [int(fly_id),
+                       int(expt_id),
+                       fly_data['date'],
+                       expt_data['brain_area'],
+                       fly_data['genotype'],
+                       visual_input,
+                       fly_data['notes'],
+                       expt_data['notes'],
+                       expt_data['time'],
+                       fly_data['circadian_on'],
+                       fly_data['circadian_off'],
+                       fly_data['gender'],
+                       fly_data['age'],
+                       fly_data['temp'],
+                       scan_data['laser_power'],
+                       scan_data['PMT_green'],
+                       scan_data['PMT_red'],
+                       scan_data['x_dim'],
+                       scan_data['y_dim'],
+                       scan_data['z_dim'],
+                       scan_data['x_voxel_size'],
+                       scan_data['y_voxel_size'],
+                       scan_data['z_voxel_size']]
 
-ws.append(new_row)
+            #if visual_data is not None:
+            #    new_row['visual_stimuli'] = visual_data
+            #if fictrac_data is not None:
+            #    new_row['fictrac'] = fictrac
 
-# Save the file
-filename='/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/master_2P_new.xlsx'
-wb.save(filename)
+            ws.append(new_row)
 
+    # Save the file
+    filename='/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/master_2P_new.xlsx'
+    wb.save(filename)
+
+if __name__ == '__main__':
+    main()
