23a385d10819da9fc6318125ac23f3cecf42eb2c
Luke Brezovec <brezovec@stanford.edu>
2019-06-27 01:55:22 -0700

    updating fly builder to work with jsons.

--- sherlock_scripts/add_fly_to_xlsx.py
+++ sherlock_scripts/add_fly_to_xlsx.py
@@ -1,27 +1,77 @@
 from openpyxl import Workbook
 from openpyxl import load_workbook
 from openpyxl.styles import Font, Fill
+import json
+import os
 
+def load_json(file):
+    with open(file, 'r') as f:
+        data = json.load(f)
+    return data
 
-wb = load_workbook(filename="2P_log_test.xlsx", read_only=False)
+filename='/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/master_2P.xlsx'
+wb = load_workbook(filename=filename, read_only=False)
+ws = wb.active
 #ws = wb['big_data']
 #wb = Workbook()
 #wb = wb.load()
-
 # grab the active worksheet
-ws = wb.active
-
-col = ws.column_dimensions['B']
-col.font = Font(bold=True)
-
+#col = ws.column_dimensions['B']
+#col.font = Font(bold=True)
 # Data can be assigned directly to cells
-ws['A1'] = 42
+#ws['A1'] = 42
 
 # Rows can also be appended
-ws.append([1, 2, 3])
+fly_folder = '/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/fly_1'
+fly_file = os.path.join(fly_folder, 'fly.json')
+fly_data = load_json(fly_file)
 
-col = ws.column_dimensions['B']
-col.font = Font(bold=True)
+expt_file = os.path.join(fly_folder, 'func_0', 'expt.json')
+expt_data = load_json(expt_file)
+
+scan_file = os.path.join(fly_folder, 'func_0', 'imaging', 'scan.json')
+scan_data = load_json(scan_file)
+
+# Get fly_id
+fly_folder = os.path.split(os.path.split(expt_folder)[0])[-1]
+fly_id = fly_folder.split('_')[-1]
+
+# Get expt_id
+expt_id = expt_folder.split('_')[-1]
+
+# Append the new row
+new_row = [fly_id,
+           expt_id,
+           fly_data['date'],
+           expt_data['brain_area'],
+           fly_data['genotype'],
+           0,
+           fly_data['notes'],
+           expt_data['notes'],
+           expt_data['time'],
+           fly_data['circadian_on'],
+           fly_data['circadian_off'],
+           fly_data['gender'],
+           fly_data['age'],
+           fly_data['temp'],
+           scan_data['power'],
+           scan_data['PMT_green'],
+           scan_data['PMT_red'],
+           scan_data['x_dim'],
+           scan_data['y_dim'],
+           scan_data['z_dim'],
+           scan_data['x_voxel_size'],
+           scan_data['y_voxel_size'],
+           scan_data['z_voxel_size']]
+
+#if visual_data is not None:
+#    new_row['visual_stimuli'] = visual_data
+#if fictrac_data is not None:
+#    new_row['fictrac'] = fictrac
+
+ws.append(new_row)
 
 # Save the file
-wb.save("2P_log_test_out.xlsx")
+filename='/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/master_2P_new.xlsx'
+wb.save(filename)
+
