3bb47a72d23e0f4f536264023db0a4e4c0d2fd5a
Luke Brezovec <brezovec@stanford.edu>
2019-06-30 18:16:00 -0700

    make visual update

--- sherlock_scripts/make_visual_json.py
+++ sherlock_scripts/make_visual_json.py
@@ -11,9 +11,10 @@ def main():
         fly_folder = os.path.join(root_path, 'fly_' + str(fly))
         visual_folders = [os.path.join(fly_folder,x,'visual') for x in os.listdir(fly_folder) if 'func' in x]
         for visual_folder in visual_folders:
-            unique_stimuli = bbb.get_stimuli(visual_folder)
-            with open(os.path.join(visual_folder, 'visual.json'), 'w') as f:
-                json.dump(unique_stimuli, f, indent=4)
+            if os.isdir(visual_folder):
+                unique_stimuli = bbb.get_stimuli(visual_folder)
+                with open(os.path.join(visual_folder, 'visual.json'), 'w') as f:
+                    json.dump(unique_stimuli, f, indent=4)
 
 if __name__ == '__main__':
     main()
