2fc657ea610e28b1c39597373b3326fc3dc043f8
lukebrez <brezovec@stanford.edu>
2019-07-12 16:27:59 -0700

    tiff stack

--- scripts/main.py
+++ scripts/main.py
@@ -94,7 +94,7 @@ flow.convert_raw_to_tiff(full_target)
 if convert_to in ['.nii', 'nii', 'nifti']:
     flow.start_convert_tiff_collections(full_target)
 elif convert_to in ['.tiff', 'tiff', '.tif', 'tif']:
-    flow.convert_tiff_collections_to_stack(directory)
+    flow.convert_tiff_collections_to_stack(full_target)
 else:
     print('{} is an invalid convert_to variable from metadata.'.format(convert_to))
 
