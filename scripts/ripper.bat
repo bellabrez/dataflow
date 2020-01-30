cc0bdb5f6f6ba4f06b8bdb2b2977576c4a634584
lukebrez <brezovec@stanford.edu>
2020-01-30 15:23:46 -0800

    updated prairie view ripper software.

--- scripts/ripper.bat
+++ scripts/ripper.bat
@@ -2,5 +2,5 @@
 echo Started ripper.
 set FOLDER_NAME=%1
 echo Ripping from folder %FOLDER_NAME%.
-"C:\Program Files\Prairie 5.4.64.600\Prairie View\Utilities\Image-Block Ripping Utility.exe" -isf -arfwsf "%FOLDER_NAME%" -cnv
+"C:\Program Files\Prairie 5.4.64.100\Prairie View\Utilities\Image-Block Ripping Utility.exe" -isf -arfwsf "%FOLDER_NAME%" -cnv
 echo Finished ripping.
