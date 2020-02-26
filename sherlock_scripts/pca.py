38628b22fb08d72f385126757eaf054e60175290
Luke Brezovec <brezovec@stanford.edu>
2020-02-26 11:32:06 -0800

    changed numpy brain load to 32 instead of 64. tracking pca memory usage

--- sherlock_scripts/pca.py
+++ sherlock_scripts/pca.py
@@ -4,22 +4,26 @@ import sys
 import bigbadbrain as bbb
 from sklearn.decomposition import PCA
 from time import time
+import psutil
 
 def main(directory):
 
+    print_mem()
     brain_path = os.path.join(directory, 'brain_zscored_green.nii')
     brain = bbb.load_numpy_brain(brain_path)
     dims = bbb.get_dims(brain)
     print('brain shape is x,y,z,t {}'.format(brain.shape))
+    print_mem()
 
     t0 = time()
-    X = brain[:,:,:,:].reshape(-1,brain.shape[-1]).T
-    brain = None
+    X = brain.reshape(-1,brain.shape[-1]).T
     print('X is time by voxels {}'.format(X.shape))
     print('Reshape duration: {}'.format(time()-t0))
+    print_mem()
 
     t0 = time()
     pca = PCA().fit(X)
+    print_mem()
     pca_scores = pca.components_
     print('Scores is PC by voxel {}'.format(pca_scores.shape))
     pca_loadings = pca.transform(X)
@@ -37,6 +41,12 @@ def main(directory):
     save_file = os.path.join(pca_directory, 'loadings_(temporal).npy')
     np.save(save_file, pca_loadings)
     print('Saved PCA!')
+    print_mem()
+
+def print_mem():
+    memory_usage = psutil.Process(os.getpid()).memory_info().rss*10**-9
+    print('Current memory usage: {:.2f}GB'.format(memory_usage))
+    sys.stdout.flush()
 
 if __name__ == '__main__':
     main(sys.argv[1])
