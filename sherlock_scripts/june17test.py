3c652788de8a34cf9cc433f3f5d56f38dcea71f2
Luke Brezovec <brezovec@stanford.edu>
2020-06-17 13:41:22 -0700

    working on restructuring sherlock portion with a single main python script that launches all sbatch jobs asynchronously.

--- sherlock_scripts/june17test.py
+++ sherlock_scripts/june17test.py
@@ -8,20 +8,27 @@ def sbatch(job_name, command, time=1, mem=1, dep=''):
  
     sbatch_command = "sbatch -J {} -o {}.out -e {}.err -t {}:00:00 --partition=trc --open-mode=append --cpus-per-task={} --wrap='{}' {}".format(job_name, 'thisisout', job_name, time, mem, command, dep)
     sbatch_response = subprocess.getoutput(sbatch_command)
-    print(sbatch_response) 
+    print(sbatch_response)
     job_id = sbatch_response.split(' ')[-1].strip()
     return job_id
 
-sys.stdout = open('hellotoyou.txt', 'w')
+#sys.stdout = open('hellotoyou.txt', 'w')
 
-command = 'ml python/3.6.1; python3 /home/users/brezovec/projects/dataflow/sherlock_scripts/june17test_minion.py'
+command = 'ml python/3.6.1; python3 /home/users/brezovec/projects/dataflow/sherlock_scripts/june17test_minion.py {}'.format('a')
 job_id = sbatch('luke_test', command)
-
 print('job_id = {}'.format(job_id))
 
+time.sleep(30)
 
-for i in range(20):
-    test = subprocess.getoutput('sacct -X -j {} --format=State'.format(job_id))
-    print('main says {}'.format(test))
-    sys.stdout.flush()
-    time.sleep(5)
+command = 'ml python/3.6.1; python3 /home/users/brezovec/projects/dataflow/sherlock_scripts/june17test_minion.py {}'.format('b')
+job_id = sbatch('luke_test', command)
+print('job_id = {}'.format(job_id))
+
+time.sleep(30)
+
+# for i in range(20):
+#     test = subprocess.getoutput('sacct -X -j {} --format=State'.format(job_id))
+#     print('main says {}'.format(test))
+#     sys.stdout.flush()
+#     time.sleep(5)
+
--- sherlock_scripts/june17test_minion.py
+++ sherlock_scripts/june17test_minion.py
@@ -1,9 +1,14 @@
 import time
 import sys
 
-sys.stdout = open('hellotoyou.txt', 'w')
+def main(args):
 
-for i in range(10):
-    print('minion says {}'.format(i))
-    sys.stdout.flush()
-    time.sleep(1)
+    sys.stdout = open('hellotoyou.txt', 'w')
+
+    for i in range(10):
+        print('minion says {} {}'.format(args[0], i))
+        sys.stdout.flush()
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
