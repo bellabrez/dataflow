4aa738c2eb6286192c64c1f954c8e546aa34f25a
Luke Brezovec <brezovec@stanford.edu>
2020-06-17 14:32:03 -0700

    working on restructuring sherlock portion with a single main python script that launches all sbatch jobs asynchronously.

--- sherlock_scripts/june17test_minion.py
+++ sherlock_scripts/june17test_minion.py
@@ -1,15 +1,18 @@
 import time
 import sys
 import os
+import fcntl
 
 def main(args):
 
     #sys.stdout = open('hellotoyou.txt', 'a+', os.O_NONBLOCK)
-    f = open('hellotoyou.txt', 'a+', os.O_NONBLOCK)
+    #f = open('hellotoyou.txt', 'a+', os.O_NONBLOCK)
 
     for i in range(10):
-        f.write('minion says {} {}'.format(args[0], i))
-        f.flush()
+        with open('tada.txt', 'a+') as f:
+            fcntl.flock(f, fcntl.LOCK_EX)
+            f.write('minion says {} {}'.format(args[0], i))
+            fcntl.flock(f, fcntl.LOCK_UN)
         #print('minion says {} {}'.format(args[0], i))
         #sys.stdout.flush()
         time.sleep(1)
