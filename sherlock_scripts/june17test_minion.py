0b17b3bace86acd01f1730c56dd488ad1551f49c
Luke Brezovec <brezovec@stanford.edu>
2020-06-17 14:37:14 -0700

    working on restructuring sherlock portion with a single main python script that launches all sbatch jobs asynchronously.

--- sherlock_scripts/june17test_minion.py
+++ sherlock_scripts/june17test_minion.py
@@ -11,7 +11,7 @@ def main(args):
     for i in range(10):
         with open('tada.txt', 'a+') as f:
             fcntl.flock(f, fcntl.LOCK_EX)
-            f.write('minion says {} {}'.format(args[0], i))
+            f.write('minion says {} {}\n'.format(args[0], i))
             fcntl.flock(f, fcntl.LOCK_UN)
         #print('minion says {} {}'.format(args[0], i))
         #sys.stdout.flush()
