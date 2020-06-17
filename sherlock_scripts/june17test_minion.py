ea94f4d768f0cd31c5fdb444c7390130b0bcc831
Luke Brezovec <brezovec@stanford.edu>
2020-06-17 15:29:56 -0700

    working on restructuring sherlock portion with a single main python script that launches all sbatch jobs asynchronously.

--- sherlock_scripts/june17test_minion.py
+++ sherlock_scripts/june17test_minion.py
@@ -7,8 +7,9 @@ def main(args):
     printlog = getattr(flow.Printlog(logfile='tada.txt'), 'print_to_log')
 
     for i in range(10):
-        printlog('minion says {} {}\n'.format(args[0], i))
+        printlog('minion says {} {}'.format(args[0], i))
         time.sleep(1)
+        dfgkjh
 
 if __name__ == '__main__':
     main(sys.argv[1:])
