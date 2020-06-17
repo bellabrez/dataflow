1c1fb80d4fc9d6b14c8ab0c4f86b4e37d6334e94
Luke Brezovec <brezovec@stanford.edu>
2020-06-17 16:00:23 -0700

    working on restructuring sherlock portion with a single main python script that launches all sbatch jobs asynchronously.

--- sherlock_scripts/june17test.py
+++ sherlock_scripts/june17test.py
@@ -3,7 +3,7 @@ import time
 import sys
 import dataflow as flow
 
-def sbatch(job_name, command, logfile=logfile, time=1, mem=1, dep=''):
+def sbatch(job_name, command, logfile, time=1, mem=1, dep=''):
     if dep != '':
         dep = '--dependency=afterok:{} --kill-on-invalid-dep=yes '.format(dep)
  
@@ -18,11 +18,11 @@ logfile = './logs/' + strftime("%Y%m%d-%H%M%S") + '.txt'
 printlog = getattr(flow.Printlog(logfile=logfile), 'print_to_log')
 
 command = 'ml python/3.6.1; python3 /home/users/brezovec/projects/dataflow/sherlock_scripts/june17test_minion.py {} {}'.format(logfile, 'a')
-job_id = sbatch('luke_test', command)
+job_id = sbatch('luke_test', command, logfile)
 printlog('job_id = {}'.format(job_id))
 
 command = 'ml python/3.6.1; python3 /home/users/brezovec/projects/dataflow/sherlock_scripts/june17test_minion.py {} {}'.format(logfile, 'b')
-job_id = sbatch('luke_test', command)
+job_id = sbatch('luke_test', command, logfile)
 printlog('job_id = {}'.format(job_id))
 
 for i in range(20):
