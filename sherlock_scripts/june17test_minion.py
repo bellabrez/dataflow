86260ff4bc81d1c232abe1d4c2473765c82e1ade
Luke Brezovec <brezovec@stanford.edu>
2020-06-17 15:11:39 -0700

    working on restructuring sherlock portion with a single main python script that launches all sbatch jobs asynchronously.

--- sherlock_scripts/june17test_minion.py
+++ sherlock_scripts/june17test_minion.py
@@ -3,18 +3,21 @@ import sys
 import os
 import fcntl
 
-def main(args):
+class Printlog():
+    def __init__(self, logfile):
+        self.logfile = logfile
+    def print_to_log(self, message):
+        with open(self.logfile, 'a+') as f:
+            fcntl.flock(f, fcntl.LOCK_EX)
+            f.write(message)
+            fcntl.flock(f, fcntl.LOCK_UN)
 
-    #sys.stdout = open('hellotoyou.txt', 'a+', os.O_NONBLOCK)
-    #f = open('hellotoyou.txt', 'a+', os.O_NONBLOCK)
+def main(args):
+    printlog_object = Printlog(logfile='tada.txt')
+    printlog = getattr(printlog_object, 'print_to_log')
 
     for i in range(10):
-        with open('tada.txt', 'a+') as f:
-            fcntl.flock(f, fcntl.LOCK_EX)
-            f.write('minion says {} {}\n'.format(args[0], i))
-            fcntl.flock(f, fcntl.LOCK_UN)
-        #print('minion says {} {}'.format(args[0], i))
-        #sys.stdout.flush()
+        printlog('minion says {} {}\n'.format(args[0], i))
         time.sleep(1)
 
 if __name__ == '__main__':
