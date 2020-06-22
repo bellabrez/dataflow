cd7ef7db5758bb777adbf1e8d35c347c70b2bf37
Luke Brezovec <brezovec@stanford.edu>
2020-06-22 14:43:45 -0700

    syncing

--- sherlock_scripts/june17test.py
+++ sherlock_scripts/june17test.py
@@ -15,7 +15,7 @@ sys.stderr = flow.Logger_stderr_sherlock(logfile)
 ### Setup Paths ###
 ###################
 
-imports_path = '/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/imports/build_queue'
+imports_path = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/imports/build_queue"
 
 ############
 ### etc. ###
@@ -38,7 +38,8 @@ How to return values? writing to a file is probably simply the best? (write to a
 
 '''
 
-args = json.dumps({"logfile": logfile, "imports_path": imports_path})
+args = json.dumps({'logfile': logfile, 'imports_path': imports_path})
+printlog('dumps: {}'.format(args))
 command = 'ml python/3.6.1; python3 /home/users/brezovec/projects/dataflow/sherlock_scripts/check_for_flag.py {}'.format(args)
 job_id = flow.sbatch('flagchk', command, logfile, time=1, mem=1, dep='')
 flow.wait_for_job(job_id, logfile)
