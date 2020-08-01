4089fe59de34999f8310a701dc1866acfbf3b552
Luke Brezovec <brezovec@stanford.edu>
2020-08-01 12:31:10 -0700

    sync

---
+++ sherlock_scripts/loop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=dataflow
+#SBATCH --partition=trc
+#SBATCH --time=3-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --output=./logs/mainlog.out
+#SBATCH --open-mode=append
+#SBATCH --mail-type=ALL
+
+ml python/3.6.1
+date
+python3 -u /home/users/brezovec/projects/dataflow/sherlock_scripts/loop.py
+#python3 -u /home/users/brezovec/projects/dataflow/sherlock_scripts/temp_moco_test.py
