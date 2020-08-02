924604e7ee39abd1278d73500a3ae3316827f1aa
Luke Brezovec <brezovec@stanford.edu>
2020-08-02 11:17:22 -0700

    sync

--- sherlock_scripts/align_anat.py
+++ sherlock_scripts/align_anat.py
@@ -57,5 +57,25 @@ def sec_to_hms(t):
         hrs=F"{np.floor((t/3600)%60):02.0f}"
         return ':'.join([hrs, mins, secs])
 
+@contextmanager
+def stderr_redirected(to=os.devnull):
+
+    fd = sys.stderr.fileno()
+
+    def _redirect_stderr(to):
+        sys.stderr.close() # + implicit flush()
+        os.dup2(to.fileno(), fd) # fd writes to 'to' file
+        sys.stderr = os.fdopen(fd, 'w') # Python writes to fd
+
+    with os.fdopen(os.dup(fd), 'w') as old_stderr:
+        with open(to, 'w') as file:
+            _redirect_stderr(to=file)
+        try:
+            yield # allow code to be run with the redirected stdout
+        finally:
+            _redirect_stderr(to=old_stderr) # restore stdout.
+                                            # buffering and flags such as
+                                            # CLOEXEC may be different
+
 if __name__ == '__main__':
     main(json.loads(sys.argv[1]))
