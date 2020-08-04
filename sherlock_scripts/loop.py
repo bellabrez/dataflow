aa7e40a71af3b0a8357518b09ab1a019a1d7ba91
Luke Brezovec <brezovec@stanford.edu>
2020-08-03 17:27:04 -0700

    sync

--- sherlock_scripts/loop.py
+++ sherlock_scripts/loop.py
@@ -19,7 +19,10 @@ width = 120 # width of print log
 nodes = 2 # 1 or 2
 nice = True # true to lower priority of jobs. ie, other users jobs go first
 
-flies = ['fly_094']
+#flies = ['fly_094']
+flies = ['fly_087', 'fly_089', 'fly_091', 'fly_092', 'fly_093', 'fly_094', 'fly_096',
+         'fly_097', 'fly_098', 'fly_099', 'fly_100', 'fly_101', 'fly_102', 'fly_105', 'fly_106',
+         'fly_109', 'fly_110', 'fly_111']
 #flies = ['fly_' + str(x).zfill(3) for x in list(range(84,112))]
 
 #####################
