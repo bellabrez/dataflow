9f37a2a916b3c572bdd6683bdffeb4fd792bc7be
Luke Brezovec <brezovec@stanford.edu>
2020-08-03 17:16:53 -0700

    sync

--- sherlock_scripts/loop.py
+++ sherlock_scripts/loop.py
@@ -9,7 +9,7 @@ import textwrap
 import dataflow as flow
 
 #modules = 'gcc/6.3.0 python/3.6.1 py-numpy/1.14.3_py36 py-pandas/0.23.0_py36 viz py-scikit-learn/0.19.1_py36'
-modeuls = 'py-numpy/1.14.3_py36 viz py-matplotlib/2.2.2_py36 py-scipy/1.1.0_py36 py-scipystack/1.0_py36 py-pandas/0.23.0_py36'
+modules = 'py-numpy/1.14.3_py36 viz py-matplotlib/2.2.2_py36 py-scipy/1.1.0_py36 py-scipystack/1.0_py36 py-pandas/0.23.0_py36'
 
 #########################
 ### Setup preferences ###
