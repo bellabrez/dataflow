3827ce4140b46be7fccadec581db195507104e9e
Luke Brezovec <brezovec@stanford.edu>
2020-08-10 16:16:12 -0700

    sync

--- sherlock_scripts/sharpen_anat.py
+++ sherlock_scripts/sharpen_anat.py
@@ -24,10 +24,32 @@ def main(args):
     file = os.path.join(directory, 'anat_red_clean.nii') 
     brain = np.asarray(nib.load(file).get_data(), dtype='float32')
 
+    ### Blur brain and mask small values ###
+    brain_copy = brain.copy().astype('float32')
+    brain_copy = scipy.ndimage.filters.gaussian_filter(brain_copy, sigma=10)
+    threshold = triangle(brain_copy)
+    brain_copy[np.where(brain_copy < threshold/2)] = 0
+
+    ### Remove blobs outside contiguous brain ###
+    labels, label_nb = scipy.ndimage.label(brain_copy)
+    brain_label = np.bincount(labels.flatten())[1:].argmax()+1
+    brain_copy = brain.copy().astype('float32')
+    brain_copy[np.where(labels != brain_label)] = np.nan
+
+    ### Narrow normalization window to prevent clipping during sharpening
+    a = .3
+    b = .7
+    brain_input = a + (brain_copy)*(b-a)
+
     ### Sharpen ###
-    brain_sharp = unsharp_mask(brain, radius=10, amount=.5)
+    brain_sharp = unsharp_mask(brain_input, radius=10, amount=.5)
     brain_sharp = unsharp_mask(brain_sharp, radius=5, amount=.5)
-    brain_out = unsharp_mask(brain_sharp, radius=2, amount=.5)
+    brain_sharp = unsharp_mask(brain_sharp, radius=2, amount=.5)
+
+    ### Renormalize via quantile normalization ###
+    brain_out = quantile_transform(brain_sharp.flatten().reshape(-1, 1), n_quantiles=500, random_state=0, copy=True)
+    brain_out = brain_out.reshape(brain.shape)
+    np.nan_to_num(brain_out, copy=False)
 
     ### Save brain ###
     save_file = os.path.join(directory, 'anat_red_clean_sharp.nii')
