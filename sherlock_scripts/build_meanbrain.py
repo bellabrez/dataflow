85c2a32c35dfa97169e2aa9941fb79ec9b97ad24
Luke Brezovec <brezovec@stanford.edu>
2020-08-11 13:28:17 -0700

    sync

--- sherlock_scripts/build_meanbrain.py
+++ sherlock_scripts/build_meanbrain.py
@@ -230,9 +230,9 @@ moving_path = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/anat_templates/
 moving_fly = "seed_syn_1_mean_0810"
 moving_resolution = res_LUKE
 
-fixed_path = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/anat_templates/kevin.nii"
-fixed_fly = 'kevin'
-fixed_resolution = res_KEVIN
+fixed_path = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/anat_templates/IBNWB.nii"
+fixed_fly = 'IBNWB'
+fixed_resolution = res_IBNWB
 
 save_directory = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/anat_templates"
 if not os.path.exists(save_directory):
