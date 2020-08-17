e101852a1e84285f06cd8fdbd4becd966753475a
Luke Brezovec <brezovec@stanford.edu>
2020-08-17 13:20:43 -0700

    sync

--- sherlock_scripts/loop.py
+++ sherlock_scripts/loop.py
@@ -19,10 +19,10 @@ width = 120 # width of print log
 nodes = 2 # 1 or 2
 nice = True # true to lower priority of jobs. ie, other users jobs go first
 
-#flies = ['fly_094']
-flies = ['fly_087', 'fly_089', 'fly_091', 'fly_092', 'fly_093', 'fly_094', 'fly_096',
-         'fly_097', 'fly_098', 'fly_099', 'fly_100', 'fly_101', 'fly_102', 'fly_105', 'fly_106',
-         'fly_109', 'fly_110', 'fly_111']
+flies = ['fly_100']
+# flies = ['fly_087', 'fly_089', 'fly_091', 'fly_092', 'fly_093', 'fly_094', 'fly_096',
+#          'fly_097', 'fly_098', 'fly_099', 'fly_100', 'fly_101', 'fly_102', 'fly_105', 'fly_106',
+#          'fly_109', 'fly_110', 'fly_111']
 #flies = ['fly_' + str(x).zfill(3) for x in list(range(84,112))]
 
 #####################
@@ -58,21 +58,37 @@ printlog("")
 ### LOOP SCRIPT ###
 ###################
 
-printlog(f"\n{'   Sharpen   ':=^{width}}")
+printlog(f"\n{'   PCA   ':=^{width}}")
 job_ids = []
 for fly in flies:
-    directory = os.path.join(dataset_path, fly, 'anat_0', 'moco')
+    directory = os.path.join(dataset_path, fly, 'func_0')
     args = {'logfile': logfile, 'directory': directory}
-    script = 'sharpen_anat.py'
-    job_id = flow.sbatch(jobname='shrpanat',
+    script = 'pca.py'
+    job_id = flow.sbatch(jobname='pca',
                          script=os.path.join(scripts_path, script),
                          modules=modules,
                          args=args,
-                         logfile=logfile, time=1, mem=1, nice=nice, nodes=nodes) # 2 to 1
+                         logfile=logfile, time=4, mem=16, nice=nice, nodes=nodes) # 2 to 1
     job_ids.append(job_id)
 
 for job_id in job_ids:
     flow.wait_for_job(job_id, logfile, com_path)
+
+# printlog(f"\n{'   Sharpen   ':=^{width}}")
+# job_ids = []
+# for fly in flies:
+#     directory = os.path.join(dataset_path, fly, 'anat_0', 'moco')
+#     args = {'logfile': logfile, 'directory': directory}
+#     script = 'sharpen_anat.py'
+#     job_id = flow.sbatch(jobname='shrpanat',
+#                          script=os.path.join(scripts_path, script),
+#                          modules=modules,
+#                          args=args,
+#                          logfile=logfile, time=1, mem=1, nice=nice, nodes=nodes) # 2 to 1
+#     job_ids.append(job_id)
+
+# for job_id in job_ids:
+#     flow.wait_for_job(job_id, logfile, com_path)
 
 # save_directory = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20200803_meanbrain"
 # input_directory = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20200803_meanbrain/syn_0"
---
+++ sherlock_scripts/pca.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import numpy as np
+import argparse
+import subprocess
+import json
+import time
+from scipy.ndimage import gaussian_filter1d
+import nibabel as nib
+import bigbadbrain as bbb
+import dataflow as flow
+
+from sklearn.decomposition import PCA
+
+def main(args):
+
+    logfile = args['logfile']
+    directory = args['directory'] # full fly func path
+    printlog = getattr(flow.Printlog(logfile=logfile), 'print_to_log')
+
+    brain_path = os.path.join(directory, 'brain_zscored_green.nii')
+    printlog("Performing PCA on {}".format(brain_path))
+    brain = np.asarray(nib.load(brain_path).get_data(), dtype='float32')
+    dim_y, dim_x, dim_z, dim_t = brain.shape
+    printlog('brain shape is x,y,z,t {}'.format(brain.shape))
+    printlog('or, {} {} {} {}'.format(dim_y, dim_x, dim_z, dim_t))
+
+    t0 = time.time()
+    X = brain[:,:,:,:].reshape(-1,brain.shape[-1]).T
+    brain = None
+    printlog('X is time by voxels {}'.format(X.shape))
+    printlog('Reshape duration: {}'.format(time()-t0))
+
+    t0 = time.time()
+    pca = PCA().fit(X)
+    pca_scores = pca.components_
+    printlog('Scores is PC by voxel {}'.format(pca_scores.shape))
+    pca_loadings = pca.transform(X)
+    printlog('Loadings is time by PC {}'.format(pca_loadings.shape))
+    pca_spatial = np.reshape(pca_scores, (-1,dim_y,dim_x,dim_z))
+    printlog('Spatial is {}'.format(pca_spatial.shape))
+    printlog('PCA duration: {}'.format(time.time()-t0))
+
+    pca_directory = os.path.join(directory, 'pca')
+    if not os.path.exists(pca_directory):
+        os.mkdir(pca_directory)
+
+    save_file = os.path.join(pca_directory, 'scores_(spatial).npy')
+    np.save(save_file, pca_spatial)
+    save_file = os.path.join(pca_directory, 'loadings_(temporal).npy')
+    np.save(save_file, pca_loadings)
+    printlog('Saved PCA!')
+
+if __name__ == '__main__':
+    main(json.loads(sys.argv[1]))
