c03160f858aeef4cd94b77fbd41443a24ba92ba3
Luke Brezovec <luke@DN51stha.SUNet>
2020-11-07 10:23:50 -0800

    sync

--- sherlock_scripts/loop.py
+++ sherlock_scripts/loop.py
@@ -359,7 +359,7 @@ for fly in flies:
                          script=os.path.join(scripts_path, script),
                          modules=modules,
                          args=args,
-                         logfile=logfile, time=2, mem=12, nice=nice, nodes=nodes) # 2 to 1
+                         logfile=logfile, time=2, mem=16, nice=nice, nodes=nodes) # 2 to 1
     job_ids.append(job_id)
 
 for job_id in job_ids:
