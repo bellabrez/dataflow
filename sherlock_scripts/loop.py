77bbe6fc7f1bab1e07e8975ee00bac7a2e78d3b8
Luke Brezovec <luke@DN0a1e64db.SUNet>
2020-12-19 16:20:24 -0800

    sync

--- sherlock_scripts/loop.py
+++ sherlock_scripts/loop.py
@@ -129,7 +129,7 @@ for job in job_params:
                              script=os.path.join(scripts_path, script),
                              modules=modules,
                              args=args,
-                             logfile=logfile, time=2, mem=4, nice=nice, nodes=nodes) # 2 to 1
+                             logfile=logfile, time=2, mem=6, nice=nice, nodes=nodes) # 2 to 1
         job_ids.append(job_id)
 
 for job_id in job_ids:
