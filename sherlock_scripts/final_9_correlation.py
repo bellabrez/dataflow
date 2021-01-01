f23b95765b546246c00755010526091e0bb1d333
Luke Brezovec <luke@DN0a1e64db.SUNet>
2021-01-01 13:56:14 -0800

    sync

--- sherlock_scripts/final_9_correlation.py
+++ sherlock_scripts/final_9_correlation.py
@@ -16,6 +16,101 @@ import dataflow as flow
 
 def main(args):
 
+    class Fly:
+        def __init__ (self, fly_name, fly_idx):
+            self.dir = os.path.join(dataset_path, fly_name, 'func_0')
+            self.fly_idx = fly_idx
+            self.fly_name = fly_name
+            self.maps = {}
+        def load_timestamps (self):
+            self.timestamps = bbb.load_timestamps(os.path.join(self.dir, 'imaging'))
+        def load_fictrac (self):
+            self.fictrac = Fictrac(self.dir, self.timestamps)
+        def load_brain_slice (self):
+            self.brain = brain[:,:,:,self.fly_idx]
+        def load_anatomy (self):
+            to_load = os.path.join(dataset_path, self.fly_name, 'warp', 'anat-to-meanbrain.nii')
+            self.anatomy = np.array(nib.load(to_load).get_data(), copy=True)
+        def get_cluster_averages (self, cluster_model_labels, n_clusters):
+            neural_data = self.brain.reshape(-1, 3384)
+            signals = []
+            self.cluster_indicies = []
+            for cluster_num in range(n_clusters):
+                cluster_indicies = np.where(cluster_model_labels==cluster_num)[0]
+                mean_signal = np.mean(neural_data[cluster_indicies,:], axis=0)
+                signals.append(mean_signal)
+                self.cluster_indicies.append(cluster_indicies) # store for later
+            self.cluster_signals=np.asarray(signals)
+        def get_cluster_id (self, x, y):
+            ax_vec = x*128 + y
+            for i in range(n_clusters):
+                if ax_vec in self.cluster_indicies[i]:
+                    cluster_id = i
+                    break
+            return cluster_id
+
+    class Fictrac:
+        def __init__ (self, fly_dir, timestamps):
+            self.fictrac_raw = bbb.load_fictrac(os.path.join(fly_dir, 'fictrac'))
+            self.timestamps = timestamps
+        def make_interp_object(self, behavior):
+            # Create camera timepoints
+            fps=50
+            camera_rate = 1/fps * 1000 # camera frame rate in ms
+            expt_len = 1000*30*60
+            x_original = np.arange(0,expt_len,camera_rate)
+
+            # Smooth raw fictrac data
+            fictrac_smoothed = scipy.signal.savgol_filter(np.asarray(self.fictrac_raw[behavior]),25,3)
+
+            # Create interp object with camera timepoints
+            fictrac_interp_object = interp1d(x_original, fictrac_smoothed, bounds_error = False)
+            return fictrac_interp_object
+
+        def pull_from_interp_object(self, interp_object, timepoints):
+            new_interp = interp_object(timepoints)
+            np.nan_to_num(new_interp, copy=False);
+            return new_interp
+
+        def interp_fictrac(self, z):
+            behaviors = ['dRotLabY', 'dRotLabZ']; shorts = ['Y', 'Z']
+            self.fictrac = {}
+
+            for behavior, short in zip(behaviors, shorts):
+                interp_object = self.make_interp_object(behavior)
+                self.fictrac[short + 'i'] = interp_object
+
+                ### Velocity ###
+                low_res_behavior = self.pull_from_interp_object(interp_object, self.timestamps[:,z])
+                self.fictrac[short] = low_res_behavior#/np.std(low_res_behavior)
+                
+                ### Clipped Velocities ###
+                self.fictrac[short + '_pos'] = np.clip(self.fictrac[short], a_min=0, a_max=None)
+                self.fictrac[short + '_neg'] = np.clip(self.fictrac[short], a_min=None, a_max=0)*-1
+                
+                ### Strongly Clipped Velocities ###
+                # excludes points even close to 0
+                #self.fictrac[short + '_pos_very'] = np.clip(self.fictrac[short], a_min=0.3, a_max=None)
+                #self.fictrac[short + '_neg_very'] = np.clip(self.fictrac[short], a_min=None, a_max=-0.3)*-1
+
+                ### Acceleration ###
+                high_res_behavior = self.pull_from_interp_object(interp_object, high_res_timepoints)
+                self.fictrac[short + 'h'] = high_res_behavior/np.std(high_res_behavior)
+
+                accel = scipy.signal.savgol_filter(np.diff(high_res_behavior),25,3)
+                accel = np.append(accel, 0)
+                interp_object = interp1d(high_res_timepoints, accel, bounds_error = False)
+                acl = interp_object(self.timestamps[:,z])
+                acl[-1] = 0
+                self.fictrac[short + 'a'] = acl#/np.std(acl)
+                
+                ### Clipped Acceleration ###
+                self.fictrac[short + 'a' + '_pos'] = np.clip(self.fictrac[short + 'a'], a_min=0, a_max=None)
+                self.fictrac[short + 'a' + '_neg'] = np.clip(self.fictrac[short + 'a'], a_min=None, a_max=0)*-1
+
+            self.fictrac['YZ'] = np.sqrt(np.power(self.fictrac['Y'],2), np.power(self.fictrac['Z'],2))
+            self.fictrac['YZh'] = np.sqrt(np.power(self.fictrac['Yh'],2), np.power(self.fictrac['Zh'],2))
+
     logfile = args['logfile']
     save_directory = args['save_directory']
     z = args['z']
@@ -117,101 +212,6 @@ def main(args):
 
     save_file = os.path.join(save_directory, 'pvaluesmanual_{}_z{}'.format(behavior_to_corr, z))
     np.save(save_file, np.asarray(p_values_t_test))
-
-    class Fly:
-        def __init__ (self, fly_name, fly_idx):
-            self.dir = os.path.join(dataset_path, fly_name, 'func_0')
-            self.fly_idx = fly_idx
-            self.fly_name = fly_name
-            self.maps = {}
-        def load_timestamps (self):
-            self.timestamps = bbb.load_timestamps(os.path.join(self.dir, 'imaging'))
-        def load_fictrac (self):
-            self.fictrac = Fictrac(self.dir, self.timestamps)
-        def load_brain_slice (self):
-            self.brain = brain[:,:,:,self.fly_idx]
-        def load_anatomy (self):
-            to_load = os.path.join(dataset_path, self.fly_name, 'warp', 'anat-to-meanbrain.nii')
-            self.anatomy = np.array(nib.load(to_load).get_data(), copy=True)
-        def get_cluster_averages (self, cluster_model_labels, n_clusters):
-            neural_data = self.brain.reshape(-1, 3384)
-            signals = []
-            self.cluster_indicies = []
-            for cluster_num in range(n_clusters):
-                cluster_indicies = np.where(cluster_model_labels==cluster_num)[0]
-                mean_signal = np.mean(neural_data[cluster_indicies,:], axis=0)
-                signals.append(mean_signal)
-                self.cluster_indicies.append(cluster_indicies) # store for later
-            self.cluster_signals=np.asarray(signals)
-        def get_cluster_id (self, x, y):
-            ax_vec = x*128 + y
-            for i in range(n_clusters):
-                if ax_vec in self.cluster_indicies[i]:
-                    cluster_id = i
-                    break
-            return cluster_id
-
-    class Fictrac:
-        def __init__ (self, fly_dir, timestamps):
-            self.fictrac_raw = bbb.load_fictrac(os.path.join(fly_dir, 'fictrac'))
-            self.timestamps = timestamps
-        def make_interp_object(self, behavior):
-            # Create camera timepoints
-            fps=50
-            camera_rate = 1/fps * 1000 # camera frame rate in ms
-            expt_len = 1000*30*60
-            x_original = np.arange(0,expt_len,camera_rate)
-
-            # Smooth raw fictrac data
-            fictrac_smoothed = scipy.signal.savgol_filter(np.asarray(self.fictrac_raw[behavior]),25,3)
-
-            # Create interp object with camera timepoints
-            fictrac_interp_object = interp1d(x_original, fictrac_smoothed, bounds_error = False)
-            return fictrac_interp_object
-
-        def pull_from_interp_object(self, interp_object, timepoints):
-            new_interp = interp_object(timepoints)
-            np.nan_to_num(new_interp, copy=False);
-            return new_interp
-
-        def interp_fictrac(self, z):
-            behaviors = ['dRotLabY', 'dRotLabZ']; shorts = ['Y', 'Z']
-            self.fictrac = {}
-
-            for behavior, short in zip(behaviors, shorts):
-                interp_object = self.make_interp_object(behavior)
-                self.fictrac[short + 'i'] = interp_object
-
-                ### Velocity ###
-                low_res_behavior = self.pull_from_interp_object(interp_object, self.timestamps[:,z])
-                self.fictrac[short] = low_res_behavior#/np.std(low_res_behavior)
-                
-                ### Clipped Velocities ###
-                self.fictrac[short + '_pos'] = np.clip(self.fictrac[short], a_min=0, a_max=None)
-                self.fictrac[short + '_neg'] = np.clip(self.fictrac[short], a_min=None, a_max=0)*-1
-                
-                ### Strongly Clipped Velocities ###
-                # excludes points even close to 0
-                #self.fictrac[short + '_pos_very'] = np.clip(self.fictrac[short], a_min=0.3, a_max=None)
-                #self.fictrac[short + '_neg_very'] = np.clip(self.fictrac[short], a_min=None, a_max=-0.3)*-1
-
-                ### Acceleration ###
-                high_res_behavior = self.pull_from_interp_object(interp_object, high_res_timepoints)
-                self.fictrac[short + 'h'] = high_res_behavior/np.std(high_res_behavior)
-
-                accel = scipy.signal.savgol_filter(np.diff(high_res_behavior),25,3)
-                accel = np.append(accel, 0)
-                interp_object = interp1d(high_res_timepoints, accel, bounds_error = False)
-                acl = interp_object(self.timestamps[:,z])
-                acl[-1] = 0
-                self.fictrac[short + 'a'] = acl#/np.std(acl)
-                
-                ### Clipped Acceleration ###
-                self.fictrac[short + 'a' + '_pos'] = np.clip(self.fictrac[short + 'a'], a_min=0, a_max=None)
-                self.fictrac[short + 'a' + '_neg'] = np.clip(self.fictrac[short + 'a'], a_min=None, a_max=0)*-1
-
-            self.fictrac['YZ'] = np.sqrt(np.power(self.fictrac['Y'],2), np.power(self.fictrac['Z'],2))
-            self.fictrac['YZh'] = np.sqrt(np.power(self.fictrac['Yh'],2), np.power(self.fictrac['Zh'],2))
 
 if __name__ == '__main__':
     main(json.loads(sys.argv[1]))
