db68782dea32b0dd5b2bc5b6ca779d062ab94634
Luke Brezovec <luke@DN0a1e64db.SUNet>
2021-01-03 15:10:54 -0800

    sync

--- sherlock_scripts/umap.py
+++ sherlock_scripts/umap.py
@@ -30,28 +30,30 @@ def main(args):
 	############################
 	### Load ALL Superslices ###
 	############################
-	to_umap = np.zeros((2000*49, 30456))
-	for z in range(49):
-		printlog(str(z))
-		brain_file = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20201129_super_slices/superslice_{}.nii".format(z) #<---------- !!!
-		brain = np.array(nib.load(brain_file).get_data(), copy=True)
-		fly_idx_delete = 3 #(fly_095)
-		brain = np.delete(brain, fly_idx_delete, axis=-1) #### DELETING FLY_095 ####
-		#(256, 128, 3384, 9)
-		brain = np.reshape(brain,(256*128,3384*9))
-		# (32768, 30456)
-		signals = []
-		for cluster_num in range(n_clusters):
-			cluster_indicies = np.where(cluster_model_labels[z,:]==cluster_num)[0]
-			mean_signal = np.mean(brain[cluster_indicies,:], axis=0)
-			signals.append(mean_signal)
-		signals=np.asarray(signals)
-		#(2000, 30456)
-		to_umap[z*2000:(z+1)*2000,:] = signals
+	# to_umap = np.zeros((2000*49, 30456))
+	# for z in range(49):
+	# 	printlog(str(z))
+	# 	brain_file = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20201129_super_slices/superslice_{}.nii".format(z) #<---------- !!!
+	# 	brain = np.array(nib.load(brain_file).get_data(), copy=True)
+	# 	fly_idx_delete = 3 #(fly_095)
+	# 	brain = np.delete(brain, fly_idx_delete, axis=-1) #### DELETING FLY_095 ####
+	# 	#(256, 128, 3384, 9)
+	# 	brain = np.reshape(brain,(256*128,3384*9))
+	# 	# (32768, 30456)
+	# 	signals = []
+	# 	for cluster_num in range(n_clusters):
+	# 		cluster_indicies = np.where(cluster_model_labels[z,:]==cluster_num)[0]
+	# 		mean_signal = np.mean(brain[cluster_indicies,:], axis=0)
+	# 		signals.append(mean_signal)
+	# 	signals=np.asarray(signals)
+	# 	#(2000, 30456)
+	# 	to_umap[z*2000:(z+1)*2000,:] = signals
 
-	save_file = '/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20210103_super_brain'
-	np.save(save_file, to_umap)
-	printlog('SAVED!')
+	# save_file = '/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20210103_super_brain'
+	# np.save(save_file, to_umap)
+	# printlog('SAVED!')
+	load_file = '/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20210103_super_brain/20210103_super_brain.npy'
+	to_umap = np.save(load_file)
 
 	t0 = time.time()
 	reducer = umap.UMAP(n_neighbors=60, min_dist=0)
