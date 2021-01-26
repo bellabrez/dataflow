edc325e400f1443912305a68e6a433d5ed9f404b
Luke Brezovec <luke@DN51stha.SUNet>
2021-01-26 13:41:39 -0800

    sync

---
+++ sherlock_scripts/20210125_meanbrain_creation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=bldmnbrn
+#SBATCH --partition=trc
+#SBATCH --time=1-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --open-mode=append
+
+ml python/3.6.1
+date
+python3 -u /home/users/brezovec/projects/dataflow/sherlock_scripts/20210125_meanbrain_creation.py
+##SBATCH --output=./logs/mainlog.out
