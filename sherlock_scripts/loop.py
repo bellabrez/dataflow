8a996ca2574541010ea78059905d0ed9a1df98f3
Luke Brezovec <luke@DN0a1e64db.SUNet>
2021-02-19 17:19:01 -0800

    sync

--- sherlock_scripts/loop.py
+++ sherlock_scripts/loop.py
@@ -95,20 +95,20 @@ printlog("")
 # for job_id in job_ids:
 #     flow.wait_for_job(job_id, logfile, com_path)
 
-printlog(f"\n{'   GLM - single fly single reconstructed   ':=^{width}}")
-job_ids = []
-for num_pcs in [1000]:#[1, 10, 30, 50, 100, 500, 1000, 10000, 26840]:
-    args = {'logfile': logfile,
-            'num_pcs': num_pcs}
-    script = 'instantaneous_glm_unique_single_reconstructed.py'
-    job_id = flow.sbatch(jobname='glmsingle',
-                         script=os.path.join(scripts_path, script),
-                         modules=modules,
-                         args=args,
-                         logfile=logfile, time=6, mem=8, nice=nice, nodes=nodes) # 2 to 1
-    job_ids.append(job_id)
-for job_id in job_ids:
-    flow.wait_for_job(job_id, logfile, com_path)
+# printlog(f"\n{'   GLM - single fly single reconstructed   ':=^{width}}")
+# job_ids = []
+# for num_pcs in [1000]:#[1, 10, 30, 50, 100, 500, 1000, 10000, 26840]:
+#     args = {'logfile': logfile,
+#             'num_pcs': num_pcs}
+#     script = 'instantaneous_glm_unique_single_reconstructed.py'
+#     job_id = flow.sbatch(jobname='glmsingle',
+#                          script=os.path.join(scripts_path, script),
+#                          modules=modules,
+#                          args=args,
+#                          logfile=logfile, time=6, mem=8, nice=nice, nodes=nodes) # 2 to 1
+#     job_ids.append(job_id)
+# for job_id in job_ids:
+#     flow.wait_for_job(job_id, logfile, com_path)
 
 # printlog(f"\n{'   CLUSTERING   ':=^{width}}")
 # job_ids = []
@@ -128,19 +128,19 @@ for job_id in job_ids:
 ### PCA ###
 ###########
 
-# printlog(f"\n{'   PCA   ':=^{width}}")
-# job_ids = []
-# for fly_idx in range(9):
-#     args = {'logfile': logfile, 'fly_idx': fly_idx}
-#     script = 'pca_of_final_9.py'
-#     job_id = flow.sbatch(jobname='pca',
-#                          script=os.path.join(scripts_path, script),
-#                          modules=modules,
-#                          args=args,
-#                          logfile=logfile, time=12, mem=6, nice=nice, nodes=nodes) # 2 to 1
-#     job_ids.append(job_id)
-# for job_id in job_ids:
-#     flow.wait_for_job(job_id, logfile, com_path)
+printlog(f"\n{'   PCA   ':=^{width}}")
+job_ids = []
+for fly_idx in range(9):
+    args = {'logfile': logfile, 'fly_idx': fly_idx}
+    script = 'pca_of_final_9.py'
+    job_id = flow.sbatch(jobname='pca',
+                         script=os.path.join(scripts_path, script),
+                         modules=modules,
+                         args=args,
+                         logfile=logfile, time=12, mem=12, nice=nice, nodes=nodes) # 2 to 1
+    job_ids.append(job_id)
+for job_id in job_ids:
+    flow.wait_for_job(job_id, logfile, com_path)
 
 # printlog(f"\n{'   PCA   ':=^{width}}")
 # X_type = 'five_fly'
