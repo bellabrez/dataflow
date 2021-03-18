fabf4d9e5119b049085ee1635670bee05bba20ab
Luke Brezovec <luke@DN0a1e6f2a.SUNet>
2021-03-18 11:53:39 -0700

    sync

--- sherlock_scripts/20210318_neu_weighted_beh.py
+++ sherlock_scripts/20210318_neu_weighted_beh.py
@@ -61,83 +61,18 @@ def main(args):
 					break
 			return cluster_id
 
-	# class Fictrac:
-	# 	def __init__ (self, fly_dir, timestamps):
-	# 		self.fictrac_raw = bbb.load_fictrac(os.path.join(fly_dir, 'fictrac'))
-	# 		self.timestamps = timestamps
-	# 	def make_interp_object(self, behavior):
-	# 		# Create camera timepoints
-	# 		fps=50
-	# 		camera_rate = 1/fps * 1000 # camera frame rate in ms
-	# 		expt_len = 1000*30*60
-	# 		x_original = np.arange(0,expt_len,camera_rate)
-
-	# 		# Smooth raw fictrac data
-	# 		fictrac_smoothed = scipy.signal.savgol_filter(np.asarray(self.fictrac_raw[behavior]),25,3)
-
-	# 		# Create interp object with camera timepoints
-	# 		fictrac_interp_object = interp1d(x_original, fictrac_smoothed, bounds_error = False)
-	# 		return fictrac_interp_object
-
-	# 	def pull_from_interp_object(self, interp_object, timepoints):
-	# 		new_interp = interp_object(timepoints)
-	# 		np.nan_to_num(new_interp, copy=False);
-	# 		return new_interp
-
-	# 	def interp_fictrac(self, z):
-	# 		behaviors = ['dRotLabY', 'dRotLabZ']; shorts = ['Y', 'Z']
-	# 		self.fictrac = {}
-
-	# 		for behavior, short in zip(behaviors, shorts):
-	# 			interp_object = self.make_interp_object(behavior)
-	# 			self.fictrac[short + 'i'] = interp_object
-
-	# 			### Velocity ###
-	# 			low_res_behavior = self.pull_from_interp_object(interp_object, self.timestamps[:,z])
-	# 			self.fictrac[short] = low_res_behavior#/np.std(low_res_behavior)
-
-	# 			### Clipped Velocities ###
-	# 			self.fictrac[short + '_pos'] = np.clip(self.fictrac[short], a_min=0, a_max=None)
-	# 			self.fictrac[short + '_neg'] = np.clip(self.fictrac[short], a_min=None, a_max=0)*-1
-
-	# 			### Strongly Clipped Velocities ###
-	# 			# excludes points even close to 0
-	# 			#self.fictrac[short + '_pos_very'] = np.clip(self.fictrac[short], a_min=0.3, a_max=None)
-	# 			#self.fictrac[short + '_neg_very'] = np.clip(self.fictrac[short], a_min=None, a_max=-0.3)*-1
-
-	# 			### Acceleration ###
-	# 			high_res_behavior = self.pull_from_interp_object(interp_object, high_res_timepoints)
-	# 			self.fictrac[short + 'h'] = high_res_behavior/np.std(high_res_behavior)
-
-	# 			accel = scipy.signal.savgol_filter(np.diff(high_res_behavior),25,3)
-	# 			accel = np.append(accel, 0)
-	# 			interp_object = interp1d(high_res_timepoints, accel, bounds_error = False)
-	# 			acl = interp_object(self.timestamps[:,z])
-	# 			acl[-1] = 0
-	# 			self.fictrac[short + 'a'] = acl#/np.std(acl)
-
-	# 			### Clipped Acceleration ###
-	# 			self.fictrac[short + 'a' + '_pos'] = np.clip(self.fictrac[short + 'a'], a_min=0, a_max=None)
-	# 			self.fictrac[short + 'a' + '_neg'] = np.clip(self.fictrac[short + 'a'], a_min=None, a_max=0)*-1
-
-	# 		self.fictrac['YZ'] = np.sqrt(np.power(self.fictrac['Y'],2), np.power(self.fictrac['Z'],2))
-	# 		self.fictrac['YZh'] = np.sqrt(np.power(self.fictrac['Yh'],2), np.power(self.fictrac['Zh'],2))
-
-	#######################
-	### Load Superslice ###
-	#######################
-	brain_file = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20201129_super_slices/superslice_{}.nii".format(z) #<---------- !!!
-	brain = np.array(nib.load(brain_file).get_data(), copy=True)
-	fly_idx_delete = 3 #(fly_095)
-	brain = np.delete(brain, fly_idx_delete, axis=-1) #### DELETING FLY_095 ####
+	#####################
+	### Load Master X ###
+	#####################
+	file = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20210316_neural_weighted_behavior/master_X.npy"
+	X = np.load(file)
 
 	#####################
 	### Make Clusters ###
 	#####################
 	n_clusters = 2000
 	labels_file = '/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20201129_super_slices/cluster_labels.npy'
-	cluster_model_labels = np.load(labels_file)
-	cluster_model_labels = cluster_model_labels[z,:]
+	cluster_model_labels_all = np.load(labels_file)
 
 	###################
 	### Build Flies ###
@@ -146,60 +81,66 @@ def main(args):
 	for i, fly in enumerate(fly_names):
 		flies[fly] = Fly(fly_name=fly, fly_idx=i)
 		flies[fly].load_timestamps()
-		#flies[fly].load_fictrac()
-		#flies[fly].fictrac.interp_fictrac(z)
-		flies[fly].load_brain_slice()
 		flies[fly].load_z_depth_correction()
-		flies[fly].get_cluster_averages(cluster_model_labels, n_clusters)
 
-	#####################
-	### Load Master X ###
-	#####################
-	file = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20210316_neural_weighted_behavior/master_X.npy"
-	X = np.load(file)
+	for z in range(9,49-9):
+		printlog(f"Z:{z}")
 
-	#################
-	### Main Loop ###
-	#################
-	cluster_responses = []
-	for cluster_num in range(n_clusters):
-	    if cluster_num%100 == 0:
-	        printlog(str(cluster_num))
-	    ###############################################################
-	    ### Build Y vector for a single supervoxel (with all flies) ###
-	    ###############################################################
-	    all_fly_neural = []
-	    for fly in fly_names:
-	        signal = flies[fly].cluster_signals[cluster_num,:]
-	        all_fly_neural.extend(signal)
-	    Y = np.asarray(all_fly_neural)
+		#######################
+		### Load Superslice ###
+		#######################
+		brain_file = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20201129_super_slices/superslice_{}.nii".format(z) #<---------- !!!
+		brain = np.array(nib.load(brain_file).get_data(), copy=True)
+		fly_idx_delete = 3 #(fly_095)
+		brain = np.delete(brain, fly_idx_delete, axis=-1) #### DELETING FLY_095 ####
 
-	    ###########################################
-	    ### Build the X matrix for this cluster ###
-	    ###########################################
-	    # For each fly, this cluster could have originally come from a different z-depth
-	    # Get correct original z-depth
-	    Xs_new = []
-	    for i, fly in enumerate(fly_names):
-	        cluster_indicies = flies[fly].cluster_indicies[cluster_num]
-	        z_map = flies[fly].z_correction[:,:,z].ravel()
-	        original_z = int(np.median(z_map[cluster_indicies]))
-	        Xs_new.append(X[original_z,i,:,:])
-	    Xs_new = np.asarray(Xs_new)
-	    X_cluster = np.reshape(np.moveaxis(Xs_new,0,1),(-1,30456))
+		# Get cluster responses for this slice
+		for fly in fly_names:
+			flies[fly].load_brain_slice()
+			flies[fly].get_cluster_averages(cluster_model_labels_all[z,:], n_clusters)
 
-	    ###################
-	    ### Dot Product ###
-	    ###################
-	    cluster_response = np.dot(X_cluster,Y)
-	    cluster_responses.append(cluster_response)
-	cluster_responses = np.asarray(cluster_responses)
+		#################
+		### Main Loop ###
+		#################
+		cluster_responses = []
+		for cluster_num in range(n_clusters):
+		    if cluster_num%100 == 0:
+		        printlog(str(cluster_num))
+		    ###############################################################
+		    ### Build Y vector for a single supervoxel (with all flies) ###
+		    ###############################################################
+		    all_fly_neural = []
+		    for fly in fly_names:
+		        signal = flies[fly].cluster_signals[cluster_num,:]
+		        all_fly_neural.extend(signal)
+		    Y = np.asarray(all_fly_neural)
 
-	######################
-	### Save Responses ###
-	######################
-	save_file = F"/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20210316_neural_weighted_behavior/responses_{z}"
-	np.save(save_file, cluster_responses)
+		    ###########################################
+		    ### Build the X matrix for this cluster ###
+		    ###########################################
+		    # For each fly, this cluster could have originally come from a different z-depth
+		    # Get correct original z-depth
+		    Xs_new = []
+		    for i, fly in enumerate(fly_names):
+		        cluster_indicies = flies[fly].cluster_indicies[cluster_num]
+		        z_map = flies[fly].z_correction[:,:,z].ravel()
+		        original_z = int(np.median(z_map[cluster_indicies]))
+		        Xs_new.append(X[original_z,i,:,:])
+		    Xs_new = np.asarray(Xs_new)
+		    X_cluster = np.reshape(np.moveaxis(Xs_new,0,1),(-1,30456))
+
+		    ###################
+		    ### Dot Product ###
+		    ###################
+		    cluster_response = np.dot(X_cluster,Y)
+		    cluster_responses.append(cluster_response)
+		cluster_responses = np.asarray(cluster_responses)
+
+		######################
+		### Save Responses ###
+		######################
+		save_file = F"/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20210316_neural_weighted_behavior/responses_{z}"
+		np.save(save_file, cluster_responses)
 
 if __name__ == '__main__':
     main(json.loads(sys.argv[1]))	 
