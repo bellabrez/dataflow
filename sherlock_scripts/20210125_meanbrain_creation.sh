3109cf5cbd55bd6297a96b1aae868fb258878de8
Luke Brezovec <brezovec@stanford.edu>
2022-06-27 21:02:33 -0700

    added subvol meanbrain

--- sherlock_scripts/20210125_meanbrain_creation.sh
+++ sherlock_scripts/20210125_meanbrain_creation.sh
@@ -5,8 +5,8 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --open-mode=append
+#SBATCH --output=./logs/mainlog.out
 
 ml python/3.6.1
 date
 python3 -u /home/users/brezovec/projects/dataflow/sherlock_scripts/20220627_meanbrain_creation_subvol.py
-##SBATCH --output=./logs/mainlog.out
