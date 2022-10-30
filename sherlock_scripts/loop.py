b89b4c2f822079501b92e740f6a807ecdf6e8912
Luke Brezovec <brezovec@stanford.edu>
2022-10-29 23:50:40 -0700

    code to align to chimera brain

--- sherlock_scripts/loop.py
+++ sherlock_scripts/loop.py
@@ -887,79 +887,89 @@ printlog(f"\n{'   Template Alignment   ':=^{width}}")
 # MURTHY RED: trc/data/Alex/albert_data/LC11/anat/LC11_to_FDA/brig3.nii
 # MURTHY GREEN: trc/data/Alex/albert_data/LC11/func/average_green_LC11_albert_fda.nii
 
-moving_dir = "/oak/stanford/groups/trc/data/Alex/clab_data/LC11/anat/raw"
-mimic_dir = "/oak/stanford/groups/trc/data/Alex/clab_data/LC11/func/raw"
+
 murthy_res = (.49,.49,1)
 clandinin_res = (.76,.76,1)
+for lab in ['clandinin', 'murthy']:
 
-for moving_file in os.listdir(moving_dir)[:1]:
-	moving_path = os.path.join(moving_dir, moving_file)
-	moving_fly = moving_file[:-4]
-	moving_resolution = clandinin_res
+	if lab == 'clandinin':
+		moving_dir = "/oak/stanford/groups/trc/data/Alex/clab_data/LC11/anat/raw"
+		mimic_dir = "/oak/stanford/groups/trc/data/Alex/clab_data/LC11/func/raw"
+		save_directory = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20221029_chimera/clandinin"
+		resolution = clandinin_res
+	elif lab == 'murthy':
+		moving_dir = "/oak/stanford/groups/trc/data/Alex/albert_data/LC11/anat/raw"
+		mimic_dir = "/oak/stanford/groups/trc/data/Alex/albert_data/LC11/func/raw"
+		save_directory = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20221029_chimera/murthy"
+		resolution = murthy_res
 
-	micim_file = moving_file.replace('red', 'green')
-	mimic_path = os.path.join(mimic_dir, micim_file)
-	mimic_fly = micim_file[:-4]
-	mimic_resolution = clandinin_res
+	for moving_file in os.listdir(moving_dir):
+		moving_path = os.path.join(moving_dir, moving_file)
+		moving_fly = moving_file[:-4]
+		moving_resolution = resolution
 
-	fixed_path = "/oak/stanford/groups/trc/data/Alex/albert_data/LC11_chimera/mbrain/templates/syn_1.nii"
-	fixed_fly = 'chimera'
-	fixed_resolution = clandinin_res
+		micim_file = moving_file.replace('red', 'green')
+		mimic_path = os.path.join(mimic_dir, micim_file)
+		mimic_fly = micim_file[:-4]
+		mimic_resolution = resolution
 
-	save_directory = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20221029_chimera/clandinin"
-	if not os.path.exists(save_directory):
-	    os.mkdir(save_directory)
+		fixed_path = "/oak/stanford/groups/trc/data/Alex/albert_data/LC11_chimera/mbrain/templates/syn_1.nii"
+		fixed_fly = 'chimera'
+		fixed_resolution = clandinin_res
 
-	type_of_transform = 'SyN' #'Affine' #SyN
-	flip_X = False
-	flip_Z = False
-	save_warp_params = False
-	low_res = False
-	very_low_res = False
-	iso_2um_fixed = False
-	iso_2um_moving = False
+		if not os.path.exists(save_directory):
+		    os.mkdir(save_directory)
 
-	grad_step = 0.2
-	flow_sigma = 3
-	total_sigma = 0
-	syn_sampling = 32
+		type_of_transform = 'SyN' #'Affine' #SyN
+		flip_X = False
+		flip_Z = False
+		save_warp_params = False
+		low_res = False
+		very_low_res = False
+		iso_2um_fixed = False
+		iso_2um_moving = False
 
-	job_ids = []
+		grad_step = 0.2
+		flow_sigma = 3
+		total_sigma = 0
+		syn_sampling = 32
 
-	args = {'logfile': logfile,
-	        'save_directory': save_directory,
-	        'fixed_path': fixed_path,
-	        'moving_path': moving_path,
-	        'fixed_fly': fixed_fly,
-	        'moving_fly': moving_fly,
-	        'type_of_transform': type_of_transform,
-	        'flip_X': flip_X,
-	        'flip_Z': flip_Z,
-	        'moving_resolution': moving_resolution,
-	        'fixed_resolution': fixed_resolution,
-	        'save_warp_params': save_warp_params,
-	        'low_res': low_res,
-	        'very_low_res': very_low_res,
-	        'iso_2um_fixed': iso_2um_fixed,
-			'iso_2um_moving': iso_2um_moving,
-	        'grad_step': grad_step,
-	        'flow_sigma': flow_sigma,
-	        'total_sigma': total_sigma,
-	        'syn_sampling': syn_sampling,
-	        'mimic_path': mimic_path,
-	        'mimic_fly': mimic_fly,
-	        'mimic_resolution': mimic_resolution}
+		job_ids = []
 
-	script = 'align_anat.py'
-	job_id = flow.sbatch(jobname='align',
-	                     script=os.path.join(scripts_path, script),
-	                     modules=modules,
-	                     args=args,
-	                     logfile=logfile, time=4, mem=16, nice=nice, nodes=nodes) # 2 to 1
-	job_ids.append(job_id)
+		args = {'logfile': logfile,
+		        'save_directory': save_directory,
+		        'fixed_path': fixed_path,
+		        'moving_path': moving_path,
+		        'fixed_fly': fixed_fly,
+		        'moving_fly': moving_fly,
+		        'type_of_transform': type_of_transform,
+		        'flip_X': flip_X,
+		        'flip_Z': flip_Z,
+		        'moving_resolution': moving_resolution,
+		        'fixed_resolution': fixed_resolution,
+		        'save_warp_params': save_warp_params,
+		        'low_res': low_res,
+		        'very_low_res': very_low_res,
+		        'iso_2um_fixed': iso_2um_fixed,
+				'iso_2um_moving': iso_2um_moving,
+		        'grad_step': grad_step,
+		        'flow_sigma': flow_sigma,
+		        'total_sigma': total_sigma,
+		        'syn_sampling': syn_sampling,
+		        'mimic_path': mimic_path,
+		        'mimic_fly': mimic_fly,
+		        'mimic_resolution': mimic_resolution}
 
-for job_id in job_ids:
-    flow.wait_for_job(job_id, logfile, com_path)
+		script = 'align_anat.py'
+		job_id = flow.sbatch(jobname='align',
+		                     script=os.path.join(scripts_path, script),
+		                     modules=modules,
+		                     args=args,
+		                     logfile=logfile, time=4, mem=16, nice=nice, nodes=nodes) # 2 to 1
+		job_ids.append(job_id)
+
+		for job_id in job_ids:
+		    flow.wait_for_job(job_id, logfile, com_path)
 
 ###################################################################################################################
 # printlog(f"\n{'   ZSCORE   ':=^{width}}")
