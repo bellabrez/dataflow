3957b58920e3dc085c1c62b885ae39e1c066da58
Luke Brezovec <brezovec@stanford.edu>
2023-01-17 10:33:09 -0800

    added accel X

---
+++ sherlock_scripts/20230117_create_behavior_X_matrix_acceleration.py
@@ -0,0 +1,205 @@
+import os
+import sys
+import numpy as np
+import argparse
+import subprocess
+import json
+import time
+from scipy.interpolate import interp1d
+import scipy
+import nibabel as nib
+import bigbadbrain as bbb
+import dataflow as flow
+
+def main(args):
+	logfile = args['logfile']
+	printlog = getattr(flow.Printlog(logfile=logfile), 'print_to_log')
+
+	fly_names = ['fly_087', 'fly_089', 'fly_094', 'fly_097', 'fly_098', 'fly_099', 'fly_100', 'fly_101', 'fly_105']
+	dataset_path = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset"
+	expt_len = 1000*30*60
+	resolution = 10
+	high_res_timepoints = np.arange(0,expt_len,resolution) #0 to last time at subsample res
+
+	class Fly:
+		def __init__ (self, fly_name, fly_idx):
+			self.dir = os.path.join(dataset_path, fly_name, 'func_0')
+			self.fly_idx = fly_idx
+			self.fly_name = fly_name
+			self.maps = {}
+		def load_timestamps (self):
+			self.timestamps = bbb.load_timestamps(os.path.join(self.dir, 'imaging'))
+		def load_fictrac (self):
+			self.fictrac = Fictrac(self.dir, self.timestamps)
+		def load_brain_slice (self):
+			self.brain = brain[:,:,:,self.fly_idx]
+		def load_anatomy (self):
+			to_load = os.path.join(dataset_path, self.fly_name, 'warp', 'anat-to-meanbrain.nii')
+			self.anatomy = np.array(nib.load(to_load).get_data(), copy=True)
+		def load_z_depth_correction (self):
+			to_load = os.path.join(dataset_path, self.fly_name, 'warp', '20201220_warped_z_depth.nii')
+			self.z_correction = np.array(nib.load(to_load).get_data(), copy=True)
+		def get_cluster_averages (self, cluster_model_labels, n_clusters):
+			neural_data = self.brain.reshape(-1, 3384)
+			signals = []
+			self.cluster_indicies = []
+			for cluster_num in range(n_clusters):
+				cluster_indicies = np.where(cluster_model_labels==cluster_num)[0]
+				mean_signal = np.mean(neural_data[cluster_indicies,:], axis=0)
+				signals.append(mean_signal)
+				self.cluster_indicies.append(cluster_indicies) # store for later
+			self.cluster_signals=np.asarray(signals)
+		def get_cluster_id (self, x, y):
+			ax_vec = x*128 + y
+			for i in range(n_clusters):
+				if ax_vec in self.cluster_indicies[i]:
+					cluster_id = i
+					break
+			return cluster_id
+
+	class Fictrac:
+		def __init__ (self, fly_dir, timestamps):
+			self.fictrac_raw = bbb.load_fictrac(os.path.join(fly_dir, 'fictrac'))
+			self.timestamps = timestamps
+		def make_interp_object(self, behavior):
+			# Create camera timepoints
+			fps=50
+			camera_rate = 1/fps * 1000 # camera frame rate in ms
+			expt_len = 1000*30*60
+			x_original = np.arange(0,expt_len,camera_rate)
+
+			# Smooth raw fictrac data
+			fictrac_smoothed = scipy.signal.savgol_filter(np.asarray(self.fictrac_raw[behavior]),25,3)
+
+			# Create interp object with camera timepoints
+			fictrac_interp_object = interp1d(x_original, fictrac_smoothed, bounds_error = False)
+			return fictrac_smoothed, fictrac_interp_object
+
+		def pull_from_interp_object(self, interp_object, timepoints):
+			new_interp = interp_object(timepoints)
+			np.nan_to_num(new_interp, copy=False);
+			return new_interp
+
+		def interp_fictrac(self):
+			behaviors = ['dRotLabY', 'dRotLabZ']; shorts = ['Y', 'Z']
+			self.fictrac = {}
+
+			for behavior, short in zip(behaviors, shorts):
+				raw_smoothed, interp_object = self.make_interp_object(behavior)
+				self.fictrac[short + 'i'] = interp_object
+				self.fictrac[short] = raw_smoothed
+
+		def make_walking_vector(self):
+			self.fictrac['W'] = np.zeros(len(self.fictrac['Y']))
+			YZ = np.sqrt(np.power(self.fictrac['Y']/np.std(self.fictrac['Y']),2),
+				 np.power(self.fictrac['Z']/np.std(self.fictrac['Z']),2))
+			self.fictrac['W'][np.where(YZ>.2)] = 1
+
+			fps=50
+			camera_rate = 1/fps * 1000 # camera frame rate in ms
+			expt_len = 1000*30*60
+			x_original = np.arange(0,expt_len,camera_rate)
+			self.fictrac['Wi'] = interp1d(x_original, self.fictrac['W'], bounds_error = False, kind = 'nearest')
+
+	def build_timeshifted_behavior_matrix(time_shifts, fly, z, behavior):
+		# Get correct behavior interp obj
+		if 'Z' in behavior: behavior_i = 'Zi'
+		if 'Y' in behavior: behavior_i = 'Yi'
+
+		interp_obj = flies[fly].fictrac.fictrac[behavior_i]
+
+		behavior_shifts = []
+		for shift in time_shifts:
+			fictrac_interp = interp_obj(flies[fly].timestamps[:,z]+shift)
+			fictrac_interp = np.nan_to_num(fictrac_interp)
+
+			# # Split VELOCITY in +/-
+			# if 'pos' in behavior:
+			# 	fictrac_interp = np.clip(fictrac_interp, a_min=0, a_max=None)
+			# if 'neg' in behavior:
+			# 	fictrac_interp = np.clip(fictrac_interp, a_min=None, a_max=0)*-1
+			
+			 #### CONVERT TO ACCELERATION
+			fictrac_interp = np.diff(fictrac_interp)
+			fictrac_interp = np.append(fictrac_interp, 0) #need to add a single value that was removed
+
+			# # Split ACCELERATION in +/-
+			# if 'plus' in behavior:
+			# 	fictrac_interp = np.clip(fictrac_interp, a_min=0, a_max=None)
+			# if 'minus' in behavior: 
+			# 	fictrac_interp = np.clip(fictrac_interp, a_min=None, a_max=0)*-1
+			# behavior_shifts.append(fictrac_interp)
+
+			#  #### CONVERT TO JERK
+			# fictrac_interp = np.diff(fictrac_interp)
+			# fictrac_interp = np.append(fictrac_interp, 0) #need to add a single value that was removed
+
+			# # Split JERK in +/-
+			# if 'up' in behavior:
+			# 	fictrac_interp = np.clip(fictrac_interp, a_min=0, a_max=None)
+			# if 'down' in behavior:
+			# 	fictrac_interp = np.clip(fictrac_interp, a_min=None, a_max=0)*-1
+			# behavior_shifts.append(fictrac_interp)
+
+		return time_shifts, behavior_shifts
+
+	def build_X (time_shifts, behaviors, z):
+		all_fly_shifts = []
+		for fly in fly_names:
+			all_behavior_shifts = []
+			for behavior in behaviors:
+				time_shifts, behavior_shifts = build_timeshifted_behavior_matrix(time_shifts=time_shifts,
+																				 fly=fly,
+																				 z=z,
+																				 behavior=behavior)
+				all_behavior_shifts.append(np.asarray(behavior_shifts))
+			all_behavior_shifts = np.asarray(all_behavior_shifts)
+			all_behavior_shifts = np.reshape(all_behavior_shifts, (-1,3384))
+			all_fly_shifts.append(all_behavior_shifts)
+		X = np.asarray(all_fly_shifts)
+		return X
+
+	###################
+	### Build Flies ###
+	###################
+	flies = {}
+	for i, fly in enumerate(fly_names):
+		flies[fly] = Fly(fly_name=fly, fly_idx=i)
+		flies[fly].load_timestamps()
+		flies[fly].load_fictrac()
+		flies[fly].fictrac.interp_fictrac()
+		flies[fly].fictrac.make_walking_vector()
+
+	time_shifts = list(range(-5000,5000,20)) # in ms
+	# pos/neg refers to velocity, plus/minus refers to acceleration
+	# behaviors = ['Y_pos_plus', 'Y_pos_minus',
+	# 			 'Z_pos_plus', 'Z_pos_minus',
+	# 			 'Z_neg_plus', 'Z_neg_minus']
+
+	#JERK
+	# behaviors = ['Y_pos_plus_up', 'Y_pos_plus_down',
+	# 			 'Y_pos_minus_up', 'Y_pos_minus_down',
+	# 			 'Z_pos_plus_up', 'Z_pos_plus_down']
+	# 			 # 'Z_pos_minus_up', 'Z_pos_minus_down']
+	# 			 # 'Z_neg_plus', 'Z_neg_plus',
+	# 			 # 'Z_neg_minus', 'Z_neg_minus']
+
+	behaviors = ['Y', 'Z']
+
+	############################################
+	### Build the complete X behavior matrix ###
+	############################################
+	Xs = []
+	for z in range(49):
+		printlog(str(z))
+		X = build_X(time_shifts, behaviors, z)
+		Xs.append(X)
+
+	######################
+	### Save Responses ###
+	######################
+	save_file = F"/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20210316_neural_weighted_behavior/20230117_master_X_accel_noclip"
+	np.save(save_file, np.asarray(Xs))
+
+if __name__ == '__main__':
+	main(json.loads(sys.argv[1]))	 
--- sherlock_scripts/loop.py
+++ sherlock_scripts/loop.py
@@ -82,20 +82,20 @@ printlog("")
 ### LOOP SCRIPT ###
 ###################
 
-# #########################################
-# ### NEURAL WEIGHTED BEHAVIOR - MAKE X ###
-# #########################################
-# job_ids = []
-# args = {'logfile': logfile}
-# script = '20221202_create_behavior_X_matrix_noYclip.py'
-# job_id = flow.sbatch(jobname='neuwebeh',
-#                      script=os.path.join(scripts_path, script),
-#                      modules=modules,
-#                      args=args,
-#                      logfile=logfile, time=2, mem=23, nice=nice, nodes=nodes) # 2 to 1
-# job_ids.append(job_id)
-# for job_id in job_ids:
-#     flow.wait_for_job(job_id, logfile, com_path)
+#########################################
+### NEURAL WEIGHTED BEHAVIOR - MAKE X ###
+#########################################
+job_ids = []
+args = {'logfile': logfile}
+script = '20230117_create_behavior_X_matrix_acceleration.py'
+job_id = flow.sbatch(jobname='neuwebeh',
+                     script=os.path.join(scripts_path, script),
+                     modules=modules,
+                     args=args,
+                     logfile=logfile, time=2, mem=23, nice=nice, nodes=nodes) # 2 to 1
+job_ids.append(job_id)
+for job_id in job_ids:
+    flow.wait_for_job(job_id, logfile, com_path)
 
 # printlog(f"\n{'   GLM - superfly reconstructed   ':=^{width}}")
 # job_ids = []
@@ -886,105 +886,105 @@ printlog("")
 
 ###################################################################################################################
 
-# #########################
-# ### General Alignment ###
-# #########################
+# # #########################
+# # ### General Alignment ###
+# # #########################
 
-# # murthy is 0.49 x 0.49 x 1 um
-# # clandinin is .6 .6 1
+# # # murthy is 0.49 x 0.49 x 1 um
+# # # clandinin is .6 .6 1
 
-printlog(f"\n{'   Template Alignment   ':=^{width}}")
-# #moving_path = "trc/data/Yukun/registration/mean_brain/LC11_to_FDA/brig1.nii"
+# printlog(f"\n{'   Template Alignment   ':=^{width}}")
+# # #moving_path = "trc/data/Yukun/registration/mean_brain/LC11_to_FDA/brig1.nii"
 
-# # CLANDININ RED: trc/data/Yukun/registration/mean_brain/LC11_to_FDA/brig3.nii
-# # CLANDININ GREEN: /trc/data/Alex/clab_data/LC11/func/average_green_LC11_clab_fda.nii
-# # MURTHY RED: trc/data/Alex/albert_data/LC11/anat/LC11_to_FDA/brig3.nii
-# # MURTHY GREEN: trc/data/Alex/albert_data/LC11/func/average_green_LC11_albert_fda.nii
+# # # CLANDININ RED: trc/data/Yukun/registration/mean_brain/LC11_to_FDA/brig3.nii
+# # # CLANDININ GREEN: /trc/data/Alex/clab_data/LC11/func/average_green_LC11_clab_fda.nii
+# # # MURTHY RED: trc/data/Alex/albert_data/LC11/anat/LC11_to_FDA/brig3.nii
+# # # MURTHY GREEN: trc/data/Alex/albert_data/LC11/func/average_green_LC11_albert_fda.nii
 
 
-murthy_res = (.49,.49,1)
-clandinin_res = (.76,.76,1)
-#for lab in ['clandinin', 'murthy']:
-for lab in ['murthy']: #quickly fixing a single brain
+# murthy_res = (.49,.49,1)
+# clandinin_res = (.76,.76,1)
+# #for lab in ['clandinin', 'murthy']:
+# for lab in ['murthy']: #quickly fixing a single brain
 
-	if lab == 'clandinin':
-		moving_dir = "/oak/stanford/groups/trc/data/Alex/clab_data/LC11/anat/raw"
-		mimic_dir = "/oak/stanford/groups/trc/data/Alex/clab_data/LC11/func/raw"
-		save_directory = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20221029_FDA_direct_affine/clandinin"
-		resolution = clandinin_res
-	elif lab == 'murthy':
-		moving_dir = "/oak/stanford/groups/trc/data/Alex/albert_data/LC11/anat/raw"
-		mimic_dir = "/oak/stanford/groups/trc/data/Alex/albert_data/LC11/func/raw"
-		save_directory = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20221029_FDA_direct_affine/murthy"
-		resolution = murthy_res
+# 	if lab == 'clandinin':
+# 		moving_dir = "/oak/stanford/groups/trc/data/Alex/clab_data/LC11/anat/raw"
+# 		mimic_dir = "/oak/stanford/groups/trc/data/Alex/clab_data/LC11/func/raw"
+# 		save_directory = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20221029_FDA_direct_affine/clandinin"
+# 		resolution = clandinin_res
+# 	elif lab == 'murthy':
+# 		moving_dir = "/oak/stanford/groups/trc/data/Alex/albert_data/LC11/anat/raw"
+# 		mimic_dir = "/oak/stanford/groups/trc/data/Alex/albert_data/LC11/func/raw"
+# 		save_directory = "/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20221029_FDA_direct_affine/murthy"
+# 		resolution = murthy_res
 
-	for moving_file in ["220420_LC11_vol1_local_atlas_red.nii"]:#os.listdir(moving_dir):
-		moving_path = os.path.join(moving_dir, moving_file)
-		moving_fly = moving_file[:-4]
-		moving_resolution = resolution
+# 	for moving_file in ["220420_LC11_vol1_local_atlas_red.nii"]:#os.listdir(moving_dir):
+# 		moving_path = os.path.join(moving_dir, moving_file)
+# 		moving_fly = moving_file[:-4]
+# 		moving_resolution = resolution
 
-		micim_file = moving_file.replace('red', 'green')
-		mimic_path = os.path.join(mimic_dir, micim_file)
-		mimic_fly = micim_file[:-4]
-		mimic_resolution = resolution
+# 		micim_file = moving_file.replace('red', 'green')
+# 		mimic_path = os.path.join(mimic_dir, micim_file)
+# 		mimic_fly = micim_file[:-4]
+# 		mimic_resolution = resolution
 
-		fixed_path = "/oak/stanford/groups/trc/data/Yukun/registration/mean_brain/FDA_downsampled_flip.nii"
-		fixed_fly = 'FDA'
-		fixed_resolution = clandinin_res
+# 		fixed_path = "/oak/stanford/groups/trc/data/Yukun/registration/mean_brain/FDA_downsampled_flip.nii"
+# 		fixed_fly = 'FDA'
+# 		fixed_resolution = clandinin_res
 
-		if not os.path.exists(save_directory):
-		    os.mkdir(save_directory)
+# 		if not os.path.exists(save_directory):
+# 		    os.mkdir(save_directory)
 
-		type_of_transform = 'Affine'#'SyN' #'Affine' #SyN
-		flip_X = False
-		flip_Z = False
-		save_warp_params = False
-		low_res = False
-		very_low_res = False
-		iso_2um_fixed = False
-		iso_2um_moving = False
+# 		type_of_transform = 'Affine'#'SyN' #'Affine' #SyN
+# 		flip_X = False
+# 		flip_Z = False
+# 		save_warp_params = False
+# 		low_res = False
+# 		very_low_res = False
+# 		iso_2um_fixed = False
+# 		iso_2um_moving = False
 
-		grad_step = 0.2
-		flow_sigma = 3
-		total_sigma = 0
-		syn_sampling = 32
+# 		grad_step = 0.2
+# 		flow_sigma = 3
+# 		total_sigma = 0
+# 		syn_sampling = 32
 
-		job_ids = []
+# 		job_ids = []
 
-		args = {'logfile': logfile,
-		        'save_directory': save_directory,
-		        'fixed_path': fixed_path,
-		        'moving_path': moving_path,
-		        'fixed_fly': fixed_fly,
-		        'moving_fly': moving_fly,
-		        'type_of_transform': type_of_transform,
-		        'flip_X': flip_X,
-		        'flip_Z': flip_Z,
-		        'moving_resolution': moving_resolution,
-		        'fixed_resolution': fixed_resolution,
-		        'save_warp_params': save_warp_params,
-		        'low_res': low_res,
-		        'very_low_res': very_low_res,
-		        'iso_2um_fixed': iso_2um_fixed,
-				'iso_2um_moving': iso_2um_moving,
-		        'grad_step': grad_step,
-		        'flow_sigma': flow_sigma,
-		        'total_sigma': total_sigma,
-		        'syn_sampling': syn_sampling,
-		        'mimic_path': mimic_path,
-		        'mimic_fly': mimic_fly,
-		        'mimic_resolution': mimic_resolution}
+# 		args = {'logfile': logfile,
+# 		        'save_directory': save_directory,
+# 		        'fixed_path': fixed_path,
+# 		        'moving_path': moving_path,
+# 		        'fixed_fly': fixed_fly,
+# 		        'moving_fly': moving_fly,
+# 		        'type_of_transform': type_of_transform,
+# 		        'flip_X': flip_X,
+# 		        'flip_Z': flip_Z,
+# 		        'moving_resolution': moving_resolution,
+# 		        'fixed_resolution': fixed_resolution,
+# 		        'save_warp_params': save_warp_params,
+# 		        'low_res': low_res,
+# 		        'very_low_res': very_low_res,
+# 		        'iso_2um_fixed': iso_2um_fixed,
+# 				'iso_2um_moving': iso_2um_moving,
+# 		        'grad_step': grad_step,
+# 		        'flow_sigma': flow_sigma,
+# 		        'total_sigma': total_sigma,
+# 		        'syn_sampling': syn_sampling,
+# 		        'mimic_path': mimic_path,
+# 		        'mimic_fly': mimic_fly,
+# 		        'mimic_resolution': mimic_resolution}
 
-		script = 'align_anat.py'
-		job_id = flow.sbatch(jobname='align',
-		                     script=os.path.join(scripts_path, script),
-		                     modules=modules,
-		                     args=args,
-		                     logfile=logfile, time=4, mem=16, nice=nice, nodes=nodes) # 2 to 1
-		job_ids.append(job_id)
+# 		script = 'align_anat.py'
+# 		job_id = flow.sbatch(jobname='align',
+# 		                     script=os.path.join(scripts_path, script),
+# 		                     modules=modules,
+# 		                     args=args,
+# 		                     logfile=logfile, time=4, mem=16, nice=nice, nodes=nodes) # 2 to 1
+# 		job_ids.append(job_id)
 
-		for job_id in job_ids:
-		    flow.wait_for_job(job_id, logfile, com_path)
+# 		for job_id in job_ids:
+# 		    flow.wait_for_job(job_id, logfile, com_path)
 
 ###################################################################################################################
 # printlog(f"\n{'   ZSCORE   ':=^{width}}")
