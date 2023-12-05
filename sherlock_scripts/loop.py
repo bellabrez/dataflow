f280a52bf59e94ac181a0fd650aebe86ec349929
Luke Brezovec <brezovec@stanford.edu>
2023-12-05 12:09:51 -0800

    added neu wei beh threshold

--- sherlock_scripts/loop.py
+++ sherlock_scripts/loop.py
@@ -329,7 +329,7 @@ printlog("")
 ### NEURAL WEIGHTED BEHAVIOR ###
 ################################
 job_ids = []
-for z in [20]:#range(49):
+for z in range(49):
     args = {'logfile': logfile, 'z': z}
     script = '20231205_neu_weighted_beh_threshold.py'
     job_id = flow.sbatch(jobname='neuwebeh',
