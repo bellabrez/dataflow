0654a3302830352da8f57829b9418355c8db9c18
Bella <brezovec@stanford.edu>
2024-09-11 15:19:35 -0400

    added aragon template creation

--- sherlock_scripts/meanbrain_creation.sh
+++ sherlock_scripts/meanbrain_creation.sh
@@ -8,5 +8,5 @@
 
 ml python/3.6.1
 date
-python3 -u /home/users/brezovec/projects/dataflow/sherlock_scripts/20240906_Aragon_male_meanbrain_creation.py
+python3 -u /home/users/brezovec/projects/dataflow/sherlock_scripts/20240911_Aragon_male_meanbrain_creation.py
 ##SBATCH --output=./logs/mainlog.out
