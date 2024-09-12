6627a3a9e44ef734a9984883381152ad0fc78548
Bella <brezovec@stanford.edu>
2024-09-12 16:44:31 -0400

    fda replication

--- sherlock_scripts/20240912_FDA_replication.py
+++ sherlock_scripts/20240912_FDA_replication.py
@@ -85,7 +85,7 @@ def main():
 	###   Affine_0    ###
 	moving_dir = clean_dir
 	name_out = 'affine_0'
-	name_fixed = "seed_fly91_clean_20200803.nii"
+	name_fixed = "seed_fly91_clean_20200803"
 	sharpen_output = False
 	alignment_iteration(main_dir, moving_dir, name_out, name_fixed, type_of_transform, resolution, sharpen_output)
 
